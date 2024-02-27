bc0d8d7a9b4d28eb90a49aa583abc46226964c22
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-02-27 14:42:57 -0300

    Implementação da atividade

---
+++ src/main/java/Exercicios/Intermediario43/Intermediario_12.java
@@ -0,0 +1,18 @@
+package Exercicios.Intermediario43;
+
+import java.util.Scanner;
+
+public class Intermediario_12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numeroLinhas = 4;
+
+        for (int i = 1; i <=numeroLinhas ; i++) {
+            for (int j=0; j<=numeroLinhas; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
