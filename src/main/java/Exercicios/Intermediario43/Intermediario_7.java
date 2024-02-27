753f709a55cba0ccb9ea4b17e8f03b236e426092
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-02-27 08:23:21 -0300

    exercicio de teste

--- src/main/java/Exercicios/Intermediario43/Intermediario_7.java
+++ src/main/java/Exercicios/Intermediario43/Intermediario_7.java
@@ -1,7 +1,15 @@
 package Exercicios.Intermediario43;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class Intermediario_7 {
     Scanner scanner = new Scanner(System.in);
+
+    //criar variaveis de valor de compra, porcentagem de desconto e valor final
+    //criar if falando que compras maior ou igual a 600 ganha 1% de desconto
+    //criar if falando que a cada 100 reais, a porcentagem aumenta 1%
+
+
 }
