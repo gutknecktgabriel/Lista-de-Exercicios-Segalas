408ed49f9148c8d2e68233c155f312ce5cab671b
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-02-28 07:44:20 -0300

    Pequenas alterações visuais

--- src/main/java/Exercicios/Intermediario43/Intermediario_12.java
+++ src/main/java/Exercicios/Intermediario43/Intermediario_12.java
@@ -6,7 +6,7 @@ public class Intermediario_12 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int numeroLinhas = 4;
+        int numeroLinhas = 5;
 
         for (int i = 1; i <=numeroLinhas ; i++) {
             for (int j=0; j<=numeroLinhas; j++) {
