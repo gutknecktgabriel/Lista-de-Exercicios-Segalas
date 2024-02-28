0abea2825c16ca8025e65a10022f4461184854bb
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-02-28 08:54:56 -0300

    Realizado o commit da atividade

--- src/main/java/Exercicios/Intermediario43/Intermediario_13.java
+++ src/main/java/Exercicios/Intermediario43/Intermediario_13.java
@@ -1,4 +1,23 @@
 package Exercicios.Intermediario43;
 
+import java.util.Scanner;
+
 public class Intermediario_13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int quantidadeLinha = 5;
+
+        for (int i = 1; i <= quantidadeLinha; i++) {
+            for (int j = 0; j <= quantidadeLinha; j++) {
+                System.out.print("*");
+                System.out.println("****" + "     " + "    ");
+                System.out.println("*****" + "    " + "    ");
+                System.out.println("*****" + "    " + "    ");
+                System.out.println("*    *");
+            }
+            System.out.println("");
+        }
+    }
 }
+
