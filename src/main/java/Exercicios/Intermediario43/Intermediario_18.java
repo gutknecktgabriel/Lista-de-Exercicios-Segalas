cc421e108e1886c9a650106e347bd13a95df70bd
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-02-29 11:36:27 -0300

    Realizada atividade

--- src/main/java/Exercicios/Intermediario43/Intermediario_18.java
+++ src/main/java/Exercicios/Intermediario43/Intermediario_18.java
@@ -1,2 +1,66 @@
-package Exercicios.Intermediario43;public class Intermediario_18 {
+package Exercicios.Intermediario43;
+
+import java.util.Scanner;
+
+public class Intermediario_18 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Variaveis idade, peso, altura.
+        //Equals para perguntar a cor dos olhos
+        //Equals para perguntar cor dos cabelos
+        //If pessoas com idade superior a 50 anos e inferior a 60 kg
+        //Variavel para receber o if acima
+
+
+        int idade = 0;
+        double peso = 0;
+        double altura = 0;
+        String cordosOlhos;
+        String cordosCabelos;
+        int contagemPesoeIdade = 0;
+        int idadeTotal = 0;
+        int contagemAlturaMenorQue150 = 0;
+        int contagemOlhoAzul = 0;
+        int ruivaSemOlhoAzul = 0;
+
+
+        for (int i = 1; i <= 4; i++) {
+            System.out.print("Informe a idade da pessoa " + i + ":");
+            idade = scanner.nextInt();
+
+            System.out.print("Informe o peso da pessoa " + i + ":");
+            peso = scanner.nextDouble();
+
+            System.out.print("Informe a altura da pessoa " + i + ":");
+            altura = scanner.nextDouble();
+
+            System.out.println("Informe a cor dos olhos através dos seguintes comandos \n A = Azul, P = Preto, V = Verde e C = Castanho");
+            cordosOlhos = scanner.next();
+
+            System.out.println("Agora, informe a cor dos cabelos através dos seguintes comandos \n P = Preto, C = Castanho, L = Louro e R = Ruivo");
+            cordosCabelos = scanner.next();
+
+            if (idade > 50 && peso < 60){
+                contagemPesoeIdade++;
+            } else if (altura <1.50) {
+                idadeTotal += idade;
+                contagemAlturaMenorQue150++;
+            } else if (cordosOlhos.equalsIgnoreCase("A")) {
+                    contagemOlhoAzul++;
+            } else if (cordosCabelos.equalsIgnoreCase("R")  && !cordosOlhos.equalsIgnoreCase("A")) {
+                ruivaSemOlhoAzul++;
+            }
+        }
+        System.out.println("Quantidade de pessoas com idade superior a 50 anos e peso inferior a 60 quilos: " + contagemPesoeIdade);
+
+        System.out.println("A média das idades das pessoas com altura inferior a 1,50: " + (contagemAlturaMenorQue150 + idadeTotal / 4));
+
+        System.out.println("Quantidade de pessoas ruivas que não possuem olhos azuis" + ruivaSemOlhoAzul);
+
+    }
 }
+
+
+
+
