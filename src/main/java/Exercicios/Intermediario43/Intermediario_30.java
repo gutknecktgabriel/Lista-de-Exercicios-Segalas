cbcbadff89e5a670c7986002590439d069ac67fb
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-03-04 11:43:43 -0300

    Atividade referente a media de idades

--- src/main/java/Exercicios/Intermediario43/Intermediario_30.java
+++ src/main/java/Exercicios/Intermediario43/Intermediario_30.java
@@ -1,14 +1,2 @@
-package Exercicios.Intermediario43;
-
-import java.util.Scanner;
-
-public class Intermediario_30 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int numeroDaMatricula = 0;
-        int notas = 0;
-        int frequencia = 0;
-
-    }
+package Exercicios.Intermediario43;public class Intermediario_30 {
 }
---
+++ src/main/java/Exercicios/Intermediario43/Intermediario_31.java
@@ -0,0 +1,24 @@
+package Exercicios.Intermediario43;
+
+import java.util.Scanner;
+
+public class Intermediario_31 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int idade = 0;
+        int somadeIdades = 0;
+        int mediadeIdades = 0;
+
+
+        for (int i = 1; i <= 4; i++) {
+            System.out.print("Informe a idade da pessoa " + i + ":");
+            idade = scanner.nextInt();
+
+            somadeIdades += idade;
+            mediadeIdades = somadeIdades / 4;
+        }
+        System.out.println("A média de idade é: " + mediadeIdades + " anos de idade");
+
+    }
+}
