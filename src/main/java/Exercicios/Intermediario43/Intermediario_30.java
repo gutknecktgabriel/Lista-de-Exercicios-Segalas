8778b466961d46aa5a1facaf5da05e889eaea39e
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-03-04 08:05:43 -0300

    Comecço da atividade..

---
+++ src/main/java/Exercicios/Intermediario43/Intermediario_30.java
@@ -0,0 +1,14 @@
+package Exercicios.Intermediario43;
+
+import java.util.Scanner;
+
+public class Intermediario_30 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numeroDaMatricula = 0;
+        int notas = 0;
+        int frequencia = 0;
+
+    }
+}
