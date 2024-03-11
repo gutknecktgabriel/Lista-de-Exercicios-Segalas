342e9ebf1ed2403a8698589db517761342463543
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-03-11 14:10:17 -0300

    Realiazada atividade referente a mostrar numeros primos no intervalo de um valor dado pelo usuario

--- src/main/java/Exercicios/Dificil43/Dificil_24.java
+++ src/main/java/Exercicios/Dificil43/Dificil_24.java
@@ -1,5 +1,28 @@
 package Exercicios.Dificil43;
 
-public class Dificil_25 {
-    
+import java.util.Scanner;
+
+public class Dificil_24 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int numero = 0;
+        int numDivisores = 0;
+
+        System.out.printf("Digite um número: ");
+        numero = scanner.nextInt();
+
+        for (int i = 2; i <=numero ; i++) {
+            boolean primo = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0)  {
+                    primo = false;
+                }
+            }
+            if (primo){
+                System.out.println(i);
+            }
+        }
+    }
 }
