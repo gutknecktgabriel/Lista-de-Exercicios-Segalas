65f529c406bda2af1521e76c45b6ceb9b08434b7
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-03-11 14:57:39 -0300

    Realizando começo da atividade

---
+++ src/main/java/Exercicios/Dificil43/Dificil_38.java
@@ -0,0 +1,15 @@
+package Exercicios.Dificil43;
+
+import java.util.Scanner;
+
+public class Dificil_38 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int nota_1 = 0;
+        int nota_2 = 0;
+        int num_2 = 0;
+
+        System.out.println();
+    }
+}
