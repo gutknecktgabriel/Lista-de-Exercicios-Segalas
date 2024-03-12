8e61624daab5c4f6c9a84e43a456ef5b336bd754
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-03-12 09:58:00 -0300

    .

---
+++ src/main/java/Exercicios/Dificil43/Dificil_43.java
@@ -0,0 +1,11 @@
+package Exercicios.Dificil43;
+
+import java.util.Scanner;
+
+public class Dificil_43 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
