99c4887c6cf5c598065d85c9bcc48821d88b7789
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-03-12 14:29:54 -0300

    Pequenos testes de vetor

--- src/main/java/Estrutura_Dados_Java/estrutura_dados_vetor.java
+++ src/main/java/Estrutura_Dados_Java/estrutura_dados_vetor.java
@@ -7,5 +7,22 @@ public class estrutura_dados_vetor {
         Scanner scanner = new Scanner(System.in);
 
 
+        //VETOR
+        int [] vetor = new int [15];
+        vetor [0] = 3;
+        vetor [1] = 3;
+        vetor [2] = 2;
+        vetor [3] = 6;
+        vetor [4] = 23;
+        vetor [4] = 21;
+        vetor [5] = 55;
+        vetor [6] = 4;
+        vetor [7] = 6;
+        vetor [8] = 5;
+        vetor [9] = 6;
+
+        for (int i = 0; i < vetor.length ; i++) {
+            System.out.println(vetor[i]);
+        }
     }
 }
