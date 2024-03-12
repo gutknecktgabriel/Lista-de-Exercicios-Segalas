9e0da8489f3a47d181a52a98e5b941487fb494d4
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-03-12 13:24:58 -0300

    Estudo sobre vetor

---
+++ src/main/java/Estrutura_Dados_Java/estrutura_dados_vetor.java
@@ -0,0 +1,11 @@
+package Estrutura_Dados_Java;
+
+import java.util.Scanner;
+
+public class estrutura_dados_vetor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
