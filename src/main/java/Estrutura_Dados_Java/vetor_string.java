23e63fcb20a601c0a1edc013389988878cdf048d
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-03-12 14:53:55 -0300

    Começando estudos sobre vetor de string

---
+++ src/main/java/Estrutura_Dados_Java/vetor_string.java
@@ -0,0 +1,21 @@
+package Estrutura_Dados_Java;
+
+import java.util.Scanner;
+
+public class vetor_string {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String [] estados = new String[10];
+        estados [0] = "AC";
+        estados [1] = "RN";
+        estados [2] = "SP";
+        estados [3] = "CE";
+        estados [4] = "PA";
+        estados [5] = "AM";
+        estados [6] = "AP";
+        estados [7] = "PB";
+        estados [8] = "SC";
+        estados [9] = "BA";
+    }
+}
