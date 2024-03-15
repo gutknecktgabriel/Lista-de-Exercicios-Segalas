6301b2144e5d4488f001a027044854043de2df5e
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-03-15 14:47:57 -0300

    class main carro

--- src/main/java/POO/Carro.java
+++ src/main/java/POO/Carro.java
@@ -1,2 +1,36 @@
-package POO;public class Carro {
+package POO;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Carro {
+    String marca;
+    String modelo;
+    String cor;
+    int anoDeFrabricacao;
+    boolean gasolina;
+    boolean freiar;
+
+    void status() {
+        System.out.println("Marca: " + this.marca);
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Cor do veículo: " + this.cor);
+        System.out.println("Ano de fabricação: " + this.anoDeFrabricacao);
+        System.out.println("Possui gasolina? " + this.gasolina);
+
+    }
+
+    void sairComCarro(){
+        if (gasolina == false){
+            System.out.println("Veículo sem gasolina");
+        }
+        else {
+            System.out.println("Ligando motores... Droga é o Braia");
+        }
+    }
+    void movimento(){
+        this.gasolina = true;
+    }
+    void parado (){
+        this.gasolina = false;
+    }
 }
