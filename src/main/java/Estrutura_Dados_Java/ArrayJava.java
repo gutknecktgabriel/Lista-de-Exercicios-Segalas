e38e50d35f53beeed259295327e4330d9136cda9
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-03-15 10:33:50 -0300

    .

--- src/main/java/Estrutura_Dados_Java/ArrayJava.java
+++ src/main/java/Estrutura_Dados_Java/ArrayJava.java
@@ -1,27 +1,26 @@
 package Estrutura_Dados_Java;
 
+import javax.xml.namespace.QName;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Queue;
 
 public class ArrayJava {
     public static void main(String[] args) {
 
         LinkedList<Person> linkedList = new LinkedList<>();
-
-    }{
-        Queue<Person> supermarket = new LinkedList<>();
-        supermarket.add(new Person("Alex", 21));
-        supermarket.add(new Person("Mariam", 18));
-        supermarket.add(new Person("Ali", 21));
-
-          System.out.println(supermarket.peek());
-        System.out.println(supermarket.size());
-        System.out.println(supermarket.poll());
-        System.out.println(supermarket.peek());
-        System.out.println(supermarket.poll());
-
-
+        linkedList.add(new Person("Alex", 21));
+        linkedList.add(new Person("Alexa", 43));
+        linkedList.addFirst(new Person("Alice", 21));
+        ListIterator<Person> personListIterator = linkedList.listIterator();
+        while (personListIterator.hasNext()){
+            System.out.println(personListIterator.next());
+        }
+        System.out.println();
+        while (personListIterator.hasPrevious()) {
+            System.out.println(personListIterator.previous());
+        }
     }
     static record Person(String name, int age){}
 }
