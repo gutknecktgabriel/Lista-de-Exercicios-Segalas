46b65ab3ecbeb532929072516d23ac4902a751de
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-03-15 12:09:26 -0300

    Criação do objeto caneta com suas respectivas caracterias, ações e valores

--- src/main/java/POO/Caneta.java
+++ src/main/java/POO/Caneta.java
@@ -1,4 +1,33 @@
-package Classe_Objeto;
+package POO;
 
 public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    void status() {
+        System.out.println("Modelo: " + this.modelo);
+        System.out.print("Caneta " + this.cor);
+        System.out.println(" Com ponta: " + this.ponta);
+        System.out.println("Carga: " +  carga);
+        System.out.printf("Está tampada? " + this.tampada);
+    }
+
+    void rabiscar() {
+        if (tampada == true){
+            System.out.println("Erro! impossível rabiscar tampado.");
+        }
+        else {
+            System.out.println("Rabiscando...");
+        }
+    }
+
+    void tampar() {
+        this.tampada = true;
+    }
+    void destampar() {
+        this.tampada = false;
+    }
 }
