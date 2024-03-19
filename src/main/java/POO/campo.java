a9e0907ea83e50b5ec673c82bd198910a5c16704
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-03-19 14:25:07 -0300

    verificar possivel erro ao executar o programa

--- src/main/java/POO/campo.java
+++ src/main/java/POO/campo.java
@@ -1,2 +1,22 @@
-package POO;public class campo {
+package POO;
+
+public class campo {
+    private char simbolo;
+
+    public campo() {
+        this.simbolo = ' ';
+    }
+
+    public char getSimbolo() {
+        return this.simbolo;
+    }
+
+    public void setSimbolo(char simbolo) {
+        if (this.simbolo == ' ') {
+            this.simbolo = simbolo;
+        } else {
+            System.out.println("Campo ja usado");
+        }
+    }
 }
+
