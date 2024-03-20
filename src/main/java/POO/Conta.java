c63c6034e94b8bafcce621a1d2e976f9b50f2641
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-03-20 10:48:12 -0300

    Gerada a class conta que contem informacoes como agencia, conta, saldo, depositar, etc

--- src/main/java/POO/Conta.java
+++ src/main/java/POO/Conta.java
@@ -1,2 +1,45 @@
-package POO;public class Conta {
+package POO;
+
+public abstract class Conta implements Regras{
+    String agencia;
+    String conta;
+    double saldo;
+    public Conta(String a, String c) {
+        this.setAgencia(a);
+        this.setConta(c);
+        this.setSaldo(0);
+    }
+
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+    public String getConta() {
+        return conta;
+    }
+    public void setConta (String conta) {
+        this.conta = conta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void depositar(double valor){
+        this.setSaldo(this.saldo + valor);
+    }
+
+    public String toString (){
+        return "Agencia: " + this.getAgencia() +
+                "\nConta: " + this.getConta() +
+                "\nSaldo: " + this.getSaldo();
+    }
 }
