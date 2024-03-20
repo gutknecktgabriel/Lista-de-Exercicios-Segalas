9a3204f8295b11d466187ce614e060638e49419a
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-03-20 08:48:52 -0300

    Criada a classe cachorro que utiliza a interface Terrestre

--- src/main/java/POO/Cachorro.java
+++ src/main/java/POO/Cachorro.java
@@ -1,2 +1,9 @@
-package POO;public class Cachorro {
+package POO;
+
+public class Cachorro implements Terrestre {
+
+    @Override
+    public String andar(String direcao) {
+        return "Indo ali..." + direcao + ".... wolf wolf....";
+    }
 }
