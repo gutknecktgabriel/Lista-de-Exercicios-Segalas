ece90aa3914f08af41d4d8a86558c06739e049f4
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-03-20 10:49:45 -0300

    Class poupança que recebe agencia como A e aniversario como an

--- src/main/java/POO/Poupanca.java
+++ src/main/java/POO/Poupanca.java
@@ -1,2 +1,26 @@
-package POO;public class Poupanca {
+package POO;
+
+public class Poupanca extends Conta{
+    private int aniversario;
+
+    public Poupanca(String a, String c, int an){
+        super(a, c);
+        this.setAniversario(an);
+    }
+    public double getAniversario(){
+        return aniversario;
+    }
+    public void setAniversario(int aniversario){
+        this.aniversario = aniversario;
+    }
+    public String toString (){
+        String texto = super.toString();
+        texto += "\nAniversario: " + this.getAniversario();
+        return texto;
+    }
+    public void sacarDinheiro(double valor){
+        if (valor <= super.getSaldo())
+        super.setSaldo(super.getSaldo() - valor);
+        else System.out.println("Saldo insuficiente para realizar o saque");
+    }
 }
