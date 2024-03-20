3787dedd5e2b6c2c6900eb7db5a24fde1fa9c561
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-03-20 13:56:42 -0300

    Criada a classe carro usando a interface veoiculo

--- src/main/java/POO/Interface/Carro.java
+++ src/main/java/POO/Interface/Carro.java
@@ -1,2 +1,13 @@
-package POO.Interface;public class Carro {
+package POO.Interface;
+
+public class Carro implements Veiculo{
+
+    Veiculo tesla = new Carro();
+    @Override
+    public void inicar() {
+        System.out.println("Ligando o motor...");
+    }
+    public void parar(){
+        System.out.println("Desligando motor...");
+    }
 }
