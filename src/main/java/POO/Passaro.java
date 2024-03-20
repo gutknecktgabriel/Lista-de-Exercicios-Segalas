f4c92ed5ee2b5528c08e0440120c9f4a36a07824
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-03-20 09:23:01 -0300

    Classe papagaio que recebe os metodos da interface ave

--- src/main/java/POO/Passaro.java
+++ src/main/java/POO/Passaro.java
@@ -1,2 +1,13 @@
-package POO;public class Gaivota {
+package POO;
+
+public class Passaro implements Ave{
+
+
+    @Override
+    public String voar(String direcao) {
+        return "Voando..." + direcao;
+    }
+    public String canto(String musica){
+        return " e cantando" + musica;
+    }
 }
