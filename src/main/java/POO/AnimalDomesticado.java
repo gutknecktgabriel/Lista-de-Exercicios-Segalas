0114c1f763bc264ec8ef511f8deb10a4d92d8c4f
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-03-20 08:51:55 -0300

    Main que recebe e apresenta a classe cachorro

--- src/main/java/POO/AnimalDomesticado.java
+++ src/main/java/POO/AnimalDomesticado.java
@@ -1,2 +1,9 @@
-package POO;public class AnimalDomesticado {
+package POO;
+
+public class AnimalDomesticado {
+    public static void main(String[] args) {
+        Cachorro cao = new Cachorro();
+        String resp =  cao.andar(" ao norte");
+        System.out.println(resp);
+    }
 }
