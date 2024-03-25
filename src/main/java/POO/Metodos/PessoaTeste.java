a874a32bc3a6ef8579a13c2ad8213a35c442bea0
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-03-25 11:24:50 -0300

    apresentando informações da classe Pessoa

--- src/main/java/POO/Metodos/PessoaTeste.java
+++ src/main/java/POO/Metodos/PessoaTeste.java
@@ -1,2 +1,12 @@
-package POO.Metodos;public class PessoaTeste {
+package POO.Metodos;
+
+public class PessoaTeste {
+    public static void main(String[] args) {
+
+        Pessoa p1 = new Pessoa("Gabriel", 22);
+        p1.setIdade(22); //alterando o valor da variavel
+
+
+        System.out.println(p1);
+    }
 }
