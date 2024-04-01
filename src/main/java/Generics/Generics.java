699cc59fefdf02fa96e95c4d35db7c41dccffb2d
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-01 13:52:47 -0300

    Classe que faz a integração da funcao generics

---
+++ src/main/java/Generics/Generics.java
@@ -0,0 +1,12 @@
+package Generics;
+
+public class Generics <T> {
+    private T value;
+
+    public void add(T value) {
+        this.value = value;
+    }
+    public T getValue(){
+        return this.value;
+    }
+}
--- src/main/java/Generics/GenericsTeste.java
+++
@@ -1,2 +0,0 @@
-package Generics;public class GenericsTeste {
-}
