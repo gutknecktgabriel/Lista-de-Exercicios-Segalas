eb71c6298b2610b196554b5fa1abfb3ba3ac57de
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-01 11:45:01 -0300

    Mudanças visuais aplicadas para melho visualização do codigo

--- src/main/java/POO/Exceptions/StackTrace.java
+++ src/main/java/POO/Exceptions/StackTrace.java
@@ -1,2 +1,29 @@
-package POO.Exceptions;public class StackTrace {
+package POO.Exceptions;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class StackTrace {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String palavra;
+        int numeroAleatorio = 0;
+
+        try {
+
+            System.out.printf("Infome uma palavra: ");
+            palavra = scanner.next();
+
+            System.out.printf("Agora, informe um número: ");
+            numeroAleatorio = scanner.nextInt();
+        }
+        catch (InputMismatchException e){
+            System.out.println("Erro! Informe um número");
+            e.printStackTrace();
+
+        }
+
+    }
 }
+
