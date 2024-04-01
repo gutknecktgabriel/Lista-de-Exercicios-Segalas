b062314ee402bad73ef59a5b37bf22c8413e8520
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-01 13:54:36 -0300

    Classe main que apresenta as informações obtidas dependendo do parametro utilizado

--- src/main/java/Generics/Main.java
+++ src/main/java/Generics/Main.java
@@ -1,2 +1,11 @@
-package Generics;public class mAIN {
+package Generics;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Generics <Integer> g = new Generics();
+        g.add(25);
+
+        System.out.println(g.getValue());
+    }
 }
