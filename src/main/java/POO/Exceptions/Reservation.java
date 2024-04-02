ee96e3e69fb3a0c7fdf75def14d9e608dd218bec
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-02 14:02:05 -0300

    Metodos de check-in e check-out

--- src/main/java/POO/Exceptions/Reservation.java
+++ src/main/java/POO/Exceptions/Reservation.java
@@ -1,2 +1,47 @@
-package POO.Exceptions;public class Reservation {
+package POO.Exceptions;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+    private Integer roomNumber;
+    private Date checkin;
+    private Date checkOut;
+
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    public Reservation(Integer roomNumber, Date checkin, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkin = checkin;
+        this.checkOut = checkOut;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+    public long duration(){
+        long diff = checkOut.getTime() - checkin.getTime();
+        TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public void updateDates(Date checkin, Date checkOut){
+        this.checkin = checkin;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString(){
+        return "Room"
+                +roomNumber
+                +", check-in: "
+                +sdf.format(checkin)
+                +", check-out: "
+                +sdf.format(checkOut)
+                +", "
+                +duration()
+                +" nights";
+    }
 }
