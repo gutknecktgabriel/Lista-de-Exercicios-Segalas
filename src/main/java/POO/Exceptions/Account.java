bdaf6830476739bf437753d9b69a7db435403f18
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-03 10:59:17 -0300

    Criação dos atributos e metodos que se refere a conta bancaria

---
+++ src/main/java/POO/Exceptions/Account.java
@@ -0,0 +1,61 @@
+package POO.Exceptions;
+
+public class Account {
+    private Integer nunber;
+    private String holder;
+    private Double balance;
+    private Double withdrawLimit;
+
+
+    //Criado construtor para receber os parametros
+    public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+        this.nunber = number;
+        this.holder = holder;
+        this.balance = balance;
+        this.withdrawLimit = withdrawLimit;
+    }
+
+    //METODOS GET AND SET
+    public Integer getNumber() {
+        return nunber;
+    }
+
+    public void setNumber(Integer number) {
+        this.nunber = number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Double getWithdrawLimit() {
+        return withdrawLimit;
+    }
+
+    public void setWithdrawLimit(Double withdrawLimit) {
+        this.withdrawLimit = withdrawLimit;
+    }
+
+
+    //Metodo deposit e saque
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+
+    }
+}
