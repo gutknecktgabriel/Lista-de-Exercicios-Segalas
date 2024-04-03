6c8351427d346780a69ec4eaf6a0f553ef8d1fa1
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-03 11:36:19 -0300

    Criação de uma exceção personalizada

--- src/main/java/POO/Exceptions/BussinesException.java
+++ src/main/java/POO/Exceptions/BussinesException.java
@@ -1,2 +1,9 @@
-package POO.Exceptions;public class Bussines {
+package POO.Exceptions;
+
+public class BussinesException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public BussinesException(String msg){
+        super(msg);
+    }
 }
