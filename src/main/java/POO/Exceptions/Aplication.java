3eb16ff7c38d1e170de4a39d8279f260d2647e34
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-03 11:01:08 -0300

    Aplicação dos metodos e utilização dos atributos para saber o resultado final do usuario

--- src/main/java/POO/Exceptions/Aplication.java
+++ src/main/java/POO/Exceptions/Aplication.java
@@ -1,2 +1,44 @@
-package POO.Exceptions;public class Aplication {
+package POO.Exceptions;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Aplication {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US); //Define padrão estadunidense de valor monetário
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Informe os dados da conta");
+        System.out.printf("Número da conta: ");
+        int number = scanner.nextInt();
+
+        System.out.printf("Titular: ");
+        scanner.nextLine();
+        String holder = scanner.next();
+
+        System.out.printf("Saldo inicial: ");
+        double balance = scanner.nextDouble();
+
+        System.out.printf("Limite de saque: ");
+        double withdrawLimit = scanner.nextDouble();
+
+        Account acc = new Account(number, holder, balance, withdrawLimit);
+
+        System.out.println();
+        System.out.printf("Informe uma quantia para sacar: ");
+        double amount = scanner.nextDouble();
+
+        if (amount > acc.getWithdrawLimit()) {
+            System.out.println("Erro no saque: A quantia excede o limite de saque");
+        } else if (amount > acc.getBalance()) {
+            System.out.println("Erro de saque: Saldo Insuficiente");
+        } else {
+            acc.withdraw(amount);
+            System.out.println("Novo saldo: " + acc.getBalance() + " reais");
+        }
+
+        scanner.close();
+    }
 }
