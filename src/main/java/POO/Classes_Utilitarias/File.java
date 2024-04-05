ddf390f48e775028e19c0ec6c7e9ef706f95de5b
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-05 13:19:54 -0300

    Aplicando pequenos conceitos de java.io

--- src/main/java/POO/Classes_Utilitarias/File.java
+++ src/main/java/POO/Classes_Utilitarias/File.java
@@ -1,6 +1,9 @@
 package POO.Classes_Utilitarias;
 
 import java.io.IOException;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Date;
 
 public class File {
     public static void main(String[] args) {
@@ -8,7 +11,15 @@ public class File {
 
         try {
             boolean isCreated = file.createNewFile();
-            System.out.println(isCreated);
+            System.out.println("Created " + isCreated);
+            System.out.println("path" + file.getPath());
+            System.out.println("path" + file.getAbsolutePath());
+            System.out.println("is directory " + file.isDirectory());
+            System.out.println("path" + file.isFile());
+            System.out.println("last mofied " + Instant.ofEpochMilli(file.lastModified()).atZone(ZoneId.systemDefault()));
+            boolean exists = file.exists();
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
