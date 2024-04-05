1cbe6fd670ae51d924d3d6af40932ba5a8c2bf8f
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-05 08:14:03 -0300

    Criada pequenos generics de inteiro, string e double, depois, imprimir na tela

--- src/main/java/POO/Generics/Generics.java
+++ src/main/java/POO/Generics/Generics.java
@@ -1,2 +1,28 @@
-package POO.Generics;public class Generics {
+package POO.Generics;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Generics {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Double> listaDeDouble = new ArrayList<>();
+
+        listaDeDouble.add(23.34);
+
+        List<Integer> listaDeInteiro = new ArrayList<>();
+        listaDeInteiro.add(652);
+
+        List<String> listaDeString = new ArrayList<>();
+
+        listaDeString.add("Gabriel");
+
+        System.out.println(listaDeInteiro);
+        System.out.println(listaDeString);
+        System.out.println(listaDeDouble);
+
+        
+    }
 }
