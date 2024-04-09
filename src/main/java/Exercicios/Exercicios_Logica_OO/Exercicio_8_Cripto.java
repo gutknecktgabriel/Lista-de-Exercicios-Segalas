9005272c6e93d493d73e3c1a2c6571552f13cea4
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-09 11:33:03 -0300

    criado for para percorrer a frase informada pelo usuário, tambem realizada a criação de um array char que será o alfabeto descriptografado

--- src/main/java/Exercicios/Exercicios_Logica_OO/Exercicio_8_Cripto.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Exercicio_8_Cripto.java
@@ -1,13 +1,29 @@
 package Exercicios.Exercicios_Logica_OO;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Exercicio_8_Cripto {
     public static void main(String[] args) {
-        Scanner scanner =  new Scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        String textoOriginal;
+        char[] alfabetoCripto = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
-        System.out.println("");
+        try {
+            String textoCriptografado;
+            String myStr;
+            System.out.println("Digite um texto: ");
+            myStr = scanner.nextLine();
+
+            for (int i = 0; i < myStr.length(); i++) {
+                char result = myStr.charAt(i);
+                System.out.print(result);
+
+            }
+        } catch (StringIndexOutOfBoundsException e) {
+            e.getMessage();
+        } catch (IndexOutOfBoundsException e) {
+            e.getMessage();
+        }
     }
 }
