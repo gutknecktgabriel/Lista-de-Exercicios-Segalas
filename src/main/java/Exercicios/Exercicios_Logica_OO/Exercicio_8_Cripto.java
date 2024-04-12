dceae8c23783fc53f8b3f6c19333c8d7c9433905
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-12 08:14:07 -0300

    No momento, o programa não está aprensentando a mensgem criptografada e descriptografada

--- src/main/java/Exercicios/Exercicios_Logica_OO/Exercicio_8_Cripto.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Exercicio_8_Cripto.java
@@ -1,29 +1,75 @@
 package Exercicios.Exercicios_Logica_OO;
 
-import java.util.List;
 import java.util.Scanner;
 
 public class Exercicio_8_Cripto {
+
+
+    private static char[] alfabetoCripto = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        char[] alfabetoCripto = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
-        try {
-            String textoCriptografado;
-            String myStr;
-            System.out.println("Digite um texto: ");
-            myStr = scanner.nextLine();
+        String textoUsuario1 = "gabz";
+        String resultCripto = criptografar(textoUsuario1);
+        String resultDescripto = descriptografar(resultCripto);
 
-            for (int i = 0; i < myStr.length(); i++) {
-                char result = myStr.charAt(i);
-                System.out.print(result);
 
+        System.out.printf("Texto criptografado: ");
+        retornar(resultCripto);
+        System.out.printf("Texto descriptografado: ");
+        retornar(resultDescripto);
+    }
+
+    private static void retornar(String frases) {
+
+        System.out.println(frases);
+
+        System.out.println("\n");
+    }
+
+    private static String criptografar(String frases) {
+        char[] resultCripto = new char[frases.length()];
+
+        for (int i = 0; i < resultCripto.length; i++) {
+            for (int j = 0; j < alfabetoCripto.length; j++) {
+                if (frases.charAt(i) == alfabetoCripto[j]) {
+                    if (frases.charAt(i) == 'z') {
+                        resultCripto[i] = alfabetoCripto[0];
+                    } else {
+                        resultCripto[i] = alfabetoCripto[j + 1];
+                    }
+                }
             }
-        } catch (StringIndexOutOfBoundsException e) {
-            e.getMessage();
-        } catch (IndexOutOfBoundsException e) {
-            e.getMessage();
         }
+        return String.valueOf(resultCripto.toString());
+    }
+
+    private static String descriptografar(String resultCripto) {
+        char[] descriptografada = new char[resultCripto.length()];
+        for (int i = 0; i < resultCripto.length(); i++) {
+            for (int j = 0; j < alfabetoCripto.length; j++) {
+                if (resultCripto.charAt(i) == (alfabetoCripto[j])) {
+                    descriptografada = validacaoCaracter(resultCripto, descriptografada);
+                }
+            }
+        }
+        return String.valueOf(descriptografada.toString());
+    }
+
+    private static char[] validacaoCaracter(String validacaoDoA, char[] descriptografada) {
+        try {
+            for (int i = 0; i < alfabetoCripto.length; i++) {
+                if (validacaoDoA.charAt(i) == 'a') {
+                    descriptografada[i] = alfabetoCripto[0];
+                }
+            }
+        }
+        catch (IndexOutOfBoundsException e ){
+            System.out.println(e.getMessage());
+        }
+        return descriptografada;
     }
 }
+
+
