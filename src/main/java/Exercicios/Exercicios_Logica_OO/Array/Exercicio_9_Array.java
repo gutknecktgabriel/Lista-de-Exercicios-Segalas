e36029102f8364579576960fb82cf3378d3cd9ec
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-12 14:49:20 -0300

    Realizada primeira parte da atividade

--- src/main/java/Exercicios/Exercicios_Logica_OO/Array/Exercicio_9_Array.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Array/Exercicio_9_Array.java
@@ -1,15 +1,33 @@
 package Exercicios.Exercicios_Logica_OO.Array;
 
 public class Exercicio_9_Array {
-    int[]arrayDeInteiro;
+    int[] arrayDeInteiro;
     int valorAtual;
     int valorMaximo;
     int posicaoAtual;
 
-    public void Exercicio_9_Array(int valorMaximo){
+    public void Exercicio_9_Array(int valorMaximo) {
+
         this.arrayDeInteiro = new int[valorMaximo];
+        this.valorAtual = 0;
+        this.posicaoAtual = arrayDeInteiro.length -1;
+        this.valorMaximo = valorMaximo;
 
+    }
 
+    public boolean adicionar(int number) {
+        if (posicaoAtual > valorMaximo){
+            return false;
+        }
+        else {
+            valorAtual = number;
+            arrayDeInteiro[posicaoAtual] = valorAtual;
+            posicaoAtual --;
+            return true;
+        }
+    }
+    public int calculoMedia(int caluloMedia){
 
+        return caluloMedia = valorAtual + valorMaximo / 2;
     }
 }
