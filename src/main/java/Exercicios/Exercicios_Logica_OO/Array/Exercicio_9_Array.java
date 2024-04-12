f1ed31951e45a5e7f5d2e2287b12897bac2226dc
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-12 14:11:56 -0300

    Exercicio relacionado a array

---
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Array/Exercicio_9_Array.java
@@ -0,0 +1,15 @@
+package Exercicios.Exercicios_Logica_OO.Array;
+
+public class Exercicio_9_Array {
+    int[]arrayDeInteiro;
+    int valorAtual;
+    int valorMaximo;
+    int posicaoAtual;
+
+    public void Exercicio_9_Array(int valorMaximo){
+        this.arrayDeInteiro = new int[valorMaximo];
+
+
+
+    }
+}
