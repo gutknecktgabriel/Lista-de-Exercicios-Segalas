62700959e9d91bab7eb21aa9d4b7ec207d012a46
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-15 08:42:58 -0300

    Começo da 2 parte da atividade, aplicar pergunta ao usuario e efetuar calculos

---
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Array/Exercicio_9_Controle.java
@@ -0,0 +1,13 @@
+package Exercicios.Exercicios_Logica_OO.Array;
+
+import java.util.Scanner;
+
+public class Exercicio_9_Controle extends Exercicio_9_Array {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
