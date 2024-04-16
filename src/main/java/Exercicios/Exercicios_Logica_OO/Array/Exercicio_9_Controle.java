dace4d396749b913c68db854bbf83f82c50d4465
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-16 07:48:04 -0300

    Realizada atividade com sucesso

--- src/main/java/Exercicios/Exercicios_Logica_OO/Array/Exercicio_9_Controle.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Array/Exercicio_9_Controle.java
@@ -1,13 +1,32 @@
 package Exercicios.Exercicios_Logica_OO.Array;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Exercicio_9_Controle extends Exercicio_9_Array {
+public class Exercicio_9_Controle {
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int tamanhoArray = 0;
+        int valor = 0;
+        Exercicio_9_Array test = new Exercicio_9_Array(tamanhoArray);
+
+        System.out.printf("Qual o tamanho do array que você deseja criar?");
+        tamanhoArray = scanner.nextInt();
 
 
+        for (int i = 0; i < tamanhoArray; i++) {
+            System.out.printf("Digite o valor para calcularmos a média: " );
+            valor = scanner.nextInt();
+
+            if (!test.adicionar(valor)) {
+                System.out.println("Tamanho máximo atingido!");
+                break;
+            }
+        }
+        System.out.println("Média dos valores: " + test.calculoMedia());
     }
 }
+
