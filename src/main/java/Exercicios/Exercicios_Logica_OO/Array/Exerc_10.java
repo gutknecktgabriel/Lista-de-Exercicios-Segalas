367c942f662f647b277cca0f742e3dce35444167
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-16 08:14:29 -0300

    Programa esta aceitando dois valores porem diz que a divisao esta tentando efetuar por 0

--- src/main/java/Exercicios/Exercicios_Logica_OO/Array/Exerc_10.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Array/Exerc_10.java
@@ -1,2 +1,45 @@
-package Exercicios.Exercicios_Logica_OO.Array;public class Exerc_10 {
+package Exercicios.Exercicios_Logica_OO.Array;
+
+import java.util.ArrayList;
+
+public class Exerc_10 {
+    private ArrayList<Integer> valores;
+    private int posicaoAtual;
+    private int valorMaximo;
+    private int valorAtual;
+    private int valorTotal;
+    private int calculoMedia = 0;
+
+
+    public Exerc_10(int valorMaximo) {
+
+        this.valores = new ArrayList<>();
+        this.valorMaximo = valorMaximo;
+        this.posicaoAtual = valores.toArray().length;
+        this.valorAtual = 0;
+    }
+
+    public boolean adicionar(int number) {
+
+        if (posicaoAtual > valorMaximo) {
+            return false;
+        } else {
+            valorAtual = number;
+            valores.set(posicaoAtual, valorAtual);
+            posicaoAtual--;
+            return true;
+        }
+    }
+
+    public int calculoMedia() {
+        for (int i = 0; i < valores.toArray().length; i++) {
+            valorTotal += valores.get(i);
+
+        }
+        calculoMedia = valorTotal / valores.length;
+        return calculoMedia;
+    }
 }
+
+
+
