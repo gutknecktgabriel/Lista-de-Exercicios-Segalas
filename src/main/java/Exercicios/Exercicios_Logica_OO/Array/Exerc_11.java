f4599f02c109116accd2dcbb6e6e31ec814b23e1
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-16 09:20:41 -0300

    Realizando começo da atividade, perguntando um valor ao usuario para decompotor esse valor em numeros primos

--- src/main/java/Exercicios/Exercicios_Logica_OO/Array/Exerc_11.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Array/Exerc_11.java
@@ -1,2 +1,16 @@
-package Exercicios.Exercicios_Logica_OO.Array;public class Exerc_11 {
+package Exercicios.Exercicios_Logica_OO.Array;
+
+import java.util.Scanner;
+
+public class Exerc_11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number;
+
+        System.out.print("Informe um valor inteiro: ");
+        number = scanner.nextInt();
+
+
+    }
 }
