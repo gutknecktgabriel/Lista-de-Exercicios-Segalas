d2f6821046de2943d95e582d5a86a88640069398
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-19 08:15:24 -0300

    Aplicado a função de apresentar em quantas tentativas o usuário acertou o número misterioso.

--- src/main/java/Exercicios/Exercicios_Logica_OO/Repeticao/Exerc_2.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Repeticao/Exerc_2.java
@@ -4,28 +4,33 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Exerc_2 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args )  {
 
+
+        Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         boolean acertou = false;
-        int tentativas = 1000;
+        int tentativas = 0;
         int numeroSecreto = random.nextInt(1000);
         long chute = 0;
 
-        while (tentativas > 0 && acertou == false) {
+
+        while (!acertou) {
 
             System.out.println("Qual seu chute? ");
             chute = scanner.nextLong();
             if (numeroSecreto == chute) {
-                System.out.println("------------------Você acertou-----------------------!!!");
+                System.out.println("Você acertou! " + "número de tentativas: " + tentativas);
                 acertou = true;
             } else if (chute < numeroSecreto) {
                 System.out.println("Valor baixo demais, tente um valor maior.");
+                tentativas++;
             } else {
                 System.out.println("Valor muito alto, tente um valor menor.");
+                tentativas++;
             }
         }
     }
+
 }
 
