cea93e8ccd831fc071e80feccbd230077d411598
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-19 08:27:37 -0300

    Correção do exercicio de verificação de números primos

--- src/main/java/Exercicios/Exercicios_Logica_OO/Repeticao/Exercicio_Repetição_1.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Repeticao/Exercicio_Repetição_1.java
@@ -1,26 +1,28 @@
 package Exercicios.Exercicios_Logica_OO.Repeticao;
 
+
 import java.util.Scanner;
 
 public class Exercicio_Repetição_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num = 0;
-        int cont = 0;
 
-        System.out.println("Digite um número para verificarmos se o mesmo é primo: ");
+        int num;
+        int numDivisores = 0;
+
+        System.out.printf("Digite um valor para verificarmos se ele é primo: ");
         num = scanner.nextInt();
 
-        for (int i = 1; i <num ; i++) {
-            if (num % i == 0){
-                cont++;
+        for (int i = 1; i <= num ; i++) {
+            if (num % i ==0){
+                numDivisores++;
             }
         }
-        if (cont == 2){
-            System.out.println("O número " + num + " não é primo");
+        if (numDivisores == 2){
+            System.out.println("O valor é primo");
         }
         else {
-            System.out.println("O número " + num + " é primo");
+            System.out.println("O valor não é primo");
         }
     }
 }
