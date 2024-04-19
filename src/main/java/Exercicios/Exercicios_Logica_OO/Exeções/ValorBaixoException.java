c98585ff881bb03c5fa7fe0924e451c5e87db213
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-19 09:44:12 -0300

    Criada classe que extende da Run time exception e lança a mensagem referente a um valor baixo demais solicitado pelo usuário

--- src/main/java/Exercicios/Exercicios_Logica_OO/Exeções/ValorBaixoException.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Exeções/ValorBaixoException.java
@@ -1,2 +1,7 @@
-package Exercicios.Exercicios_Logica_OO.Exeções;public class ValorBaixoException {
+package Exercicios.Exercicios_Logica_OO.Exeções;
+
+public class ValorBaixoException extends RuntimeException{
+    public String getMessage() {
+        return ("Valor baixo demais, tente um valor maior.");
+    }
 }
