d36a07983813ed6394c42a4efa9616da9e91bd74
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-19 13:58:02 -0300

    Criado um arquivo txt dentro da máquina

--- src/main/java/Exercicios/Exercicios_Logica_OO/Repeticao/Exerc_2.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Repeticao/Exerc_2.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 public class Exerc_2 {
     public static void main(String[] args )  {
 
-
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         boolean acertou = false;
@@ -14,13 +13,12 @@ public class Exerc_2 {
         int numeroSecreto = random.nextInt(1000);
         long chute = 0;
 
-
         while (!acertou) {
 
             System.out.println("Qual seu chute? ");
             chute = scanner.nextLong();
             if (numeroSecreto == chute) {
-                System.out.println("Você acertou! " + "número de tentativas: " + tentativas);
+                System.out.println("Você acertou! número de tentativas: " + tentativas);
                 acertou = true;
             } else if (chute < numeroSecreto) {
                 System.out.println("Valor baixo demais, tente um valor maior.");
