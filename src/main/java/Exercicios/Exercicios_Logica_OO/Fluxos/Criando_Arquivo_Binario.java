97f55e0857b6e8b2e9e43fba7cbe31595e88c17c
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-22 14:56:22 -0300

    Começando programa para ler arquivo binario

---
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Fluxos/Criando_Arquivo_Binario.java
@@ -0,0 +1,14 @@
+package Exercicios.Exercicios_Logica_OO.Fluxos;
+
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class Criando_Arquivo_Binario {
+    public static void main(String[] args) throws Exception {
+
+        try {
+
+            OutputStream stream = new FileOutputStream("data.bin");
+        }
+    }
+}
