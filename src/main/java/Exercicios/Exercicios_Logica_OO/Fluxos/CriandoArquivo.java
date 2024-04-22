81ce8a3a900c25314b7836c0f8806b6471961b4d
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-22 13:45:33 -0300

    Criado um arquivo do tipo txt

--- src/main/java/Exercicios/Exercicios_Logica_OO/Fluxos/CriandoArquivo.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Fluxos/CriandoArquivo.java
@@ -1,2 +1,21 @@
-package Exercicios.Exercicios_Logica_OO.Fluxos;public class CriandoArquivi {
+package Exercicios.Exercicios_Logica_OO.Fluxos;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CriandoArquivo {
+    public static void main(String[] args) {
+        try {
+            File file = new File("criandoArquivo.txt");
+            if (file.createNewFile()){
+                System.out.println("Arquivo criado: " + file.getName());
+            }
+            else {
+                System.out.println("Arquivo já existente.");
+            }
+        } catch (IOException e) {
+            System.out.println("Um erro aconteceu: ");
+            e.printStackTrace();
+        }
+    }
 }
