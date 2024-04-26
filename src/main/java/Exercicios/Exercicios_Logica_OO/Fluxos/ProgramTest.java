b56ce3f94dee161db1f8477ce4a251a76f7bd76c
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-26 08:39:31 -0300

    Programa que irá recber os dados de produto, preço e nome e armazenar em um arquivo csv

--- src/main/java/Exercicios/Exercicios_Logica_OO/Fluxos/ProgramTest.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Fluxos/ProgramTest.java
@@ -1,2 +1,26 @@
-package Exercicios.Exercicios_Logica_OO.Fluxos;public class ProgramTest {
+package Exercicios.Exercicios_Logica_OO.Fluxos;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ProgramTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Product> list = new ArrayList<>();
+
+        System.out.print("Enter file path: ");
+        String sourceFileStr = scanner.nextLine();
+
+        File sourceFile = new File(sourceFileStr);
+        String sourceFolderStr = sourceFile.getParent();
+
+        boolean sucess = new File(sourceFolderStr + "out\\").mkdir();
+        System.out.println("Folder created: " + sucess);
+
+        String targetFileStr = sourceFolderStr + "out\\summary.csv";
+
+        scanner.close();
+    }
 }
