106d6dd38c54a51383c85b54f9ba6dbbabe5ee51
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-29 09:18:41 -0300

    Utilizando hasmap para a estrutura de dados, apresentando e utilizando o construtor da mesma

--- src/main/java/Exercicios/Exercicios_Logica_OO/DesafioLogica/BookRepository.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/DesafioLogica/BookRepository.java
@@ -1,5 +1,13 @@
 package Exercicios.Exercicios_Logica_OO.DesafioLogica;
 
+import java.util.HashMap;
+
 public class BookRepository {
+    HashMap<String, Livro> livro;
+
+    public BookRepository(HashMap<String, Livro> livro) {
+        this.livro = livro;
+    }
 
 }
+
