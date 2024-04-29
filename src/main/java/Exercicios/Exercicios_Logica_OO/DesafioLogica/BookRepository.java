1c4ca66e6f85411b5dc024cf3bbfd8c8c9a37467
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-04-29 14:35:32 -0300

    Realizando processo de adicionar livro pela chave codigo

--- src/main/java/Exercicios/Exercicios_Logica_OO/DesafioLogica/BookRepository.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/DesafioLogica/BookRepository.java
@@ -3,11 +3,20 @@ package Exercicios.Exercicios_Logica_OO.DesafioLogica;
 import java.util.HashMap;
 
 public class BookRepository {
-    HashMap<String, Livro> livro;
 
-    public BookRepository(HashMap<String, Livro> livro) {
-        this.livro = livro;
+    HashMap<String, Livro> livro = new HashMap<String, Livro>();
+
+    //Metodo para adicionar livros usando o codigo como chave
+
+    public void addBook(Livro livro) {
+        try {
+            if (livro.getCodLivro() != null) {
+                System.out.println(livro.getCodLivro().contains(livro.codLivro));
+
+            }
+        } catch (LivroNaoEncontradoException e) {
+            return;
+        }
+
     }
-
 }
-
