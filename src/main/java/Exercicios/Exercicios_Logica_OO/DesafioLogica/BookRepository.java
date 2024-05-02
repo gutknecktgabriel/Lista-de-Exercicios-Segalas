c9ad9a728f1e342734fc81a77b2904d438d3ae50
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-02 08:09:51 -0300

    Returado o meotod adicionar livro da class livro e adiciondo na class Book Repository

--- src/main/java/Exercicios/Exercicios_Logica_OO/DesafioLogica/BookRepository.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/DesafioLogica/BookRepository.java
@@ -9,10 +9,17 @@ public class BookRepository {
     public BookRepository() {
         livros = new HashMap<>();
     }
-
     //Metodo para adicionar livros usando o codigo como chave
-
     public void addBook(Livro livro) {
         livros.put(livro.getCodLivro(), livro);
+    }
+
+    //Buscar livro por codigo
+    public String buscarLivro(String codLivro) throws LivroNaoEncontradoException {
+        String livro = codLivro;
+        if (livro == null) {
+            throw new LivroNaoEncontradoException();
+        }
+        return livro;
     }
 }
