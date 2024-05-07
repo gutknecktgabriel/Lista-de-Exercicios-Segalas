0028347cb9dac69cabcaee4ea89b75502be314e9
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-07 11:54:55 -0300

    Contact

--- src/main/java/CadastroContatos/Model/Contact.java
+++ src/main/java/CadastroContatos/Model/Contact.java
@@ -1,2 +1,65 @@
-package CadastroContatos.Model;public class Contact {
+package CadastroContatos.Model;
+import java.util.Objects;
+import java.util.UUID;
+
+public class Contact {
+    private UUID id;
+    private String name;
+    private String email;
+    private String phone;
+
+    public UUID getId(){
+        return id;
+    }
+
+    public void setId(UUID id){
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Contact contact)) return false;
+        return Objects.equals(id, contact.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
+
