c7d2d5ecefbe68f123f14e8e650b97a44ecee3f8
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-07 11:54:27 -0300

    Contact Controller

--- src/main/java/CadastroContatos/Controller/ContactController.java
+++ src/main/java/CadastroContatos/Controller/ContactController.java
@@ -1,2 +1,43 @@
-package CadastroContatos.Controller;public class ContactController {
+package CadastroContatos.Controller;
+
+import CadastroContatos.Model.Contact;
+import CadastroContatos.Model.ContactRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public class ContactController {
+
+    private ContactRepository repository = new ContactRepository();
+
+    public boolean createContact(String name, String email, String phone){
+        Contact newContact = new Contact();
+        newContact.setName(name);
+        newContact.setEmail(email);
+        newContact.setPhone(phone);
+        return repository.insert(newContact);
+    }
+
+    public List<Contact> loadAll(){
+        return repository.findAll();
+    }
+
+    public List<Contact> searchContacts(String name){
+        return repository.findByName(name);
+    }
+
+    public boolean updateContact(String id, String name, String email, String phone){
+        Contact contact = repository.findById(UUID.fromString(id));
+        if(contact == null){
+            return false;
+        }
+        contact.setName(name);
+        contact.setEmail(email);
+        contact.setPhone(phone);
+        return repository.update(contact);
+    }
+
+    public boolean removeContact(String id){
+        return repository.remove(UUID.fromString(id));
+    }
 }
