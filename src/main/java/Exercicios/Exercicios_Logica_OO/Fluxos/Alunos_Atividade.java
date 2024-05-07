5cf35a7c784fb34978c21e7be3fae6b13278b9f7
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-07 08:32:15 -0300

    Atividade realizada

--- src/main/java/Exercicios/Exercicios_Logica_OO/Fluxos/Alunos_Atividade.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Fluxos/Alunos_Atividade.java
@@ -5,52 +5,70 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Alunos_Atividade {
+
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(System.in);
 
         try {
             DataOutputStream stream = new DataOutputStream(new FileOutputStream("c:\\temp\\DadosAlunos.txt"));
 
-            for (int i = 0; i <= 2; i++) {
+            int contador = 0;
+            int somaNotas = 0;
+            int numAlunos = 3;
+
+            for (int i = 0; i < numAlunos; i++) {
                 System.out.println("Informe o nome do aluno: ");
-                String nomeAlunos = null;
-                nomeAlunos = scanner.next();
-                String tamanho = String.valueOf(nomeAlunos.length());
+                String nomeAlunos = scanner.next();
 
+                System.out.println("Informe a primeira nota do aluno: ");
+                int nota_1 = scanner.nextInt();
 
-                System.out.println("Informe as notas do aluno");
-                String nota_1 = scanner.next();
-                String nota_2 = scanner.next();
+                System.out.println("Informe a segunda nota do aluno: ");
+                int nota_2 = scanner.nextInt();
 
-                System.out.println("Informe o codigo do aluno: ");
-                String codigoAlunos;
+                System.out.println("Informe o código do aluno: ");
+                String codigoAlunos = scanner.next();
 
+                stream.writeUTF(nomeAlunos);
+                stream.writeInt(nota_1);
+                stream.writeInt(nota_2);
+                stream.writeUTF(codigoAlunos);
 
-                codigoAlunos = scanner.next();
-
-                stream.write(nomeAlunos.getBytes());
-                stream.write(tamanho.getBytes());
-
-                stream.write(nota_1.getBytes());
-                stream.write(nota_2.getBytes());
-
-                stream.write(codigoAlunos.getBytes());
-
+                contador++;
+                somaNotas += nota_1 + nota_2;
             }
+
+            double mediaTurma = (double) somaNotas / (numAlunos * 2);
+            int notaMinima = 6;
+            int alunosReprovados = 0;
+            DataInputStream inputStream = new DataInputStream(new FileInputStream("c:\\temp\\DadosAlunos.txt"));
+            for (int i = 0; i < numAlunos; i++) {
+                String nomeAluno = inputStream.readUTF();
+                int nota1 = inputStream.readInt();
+                int nota2 = inputStream.readInt();
+                String codigo = inputStream.readUTF();
+                double media = (nota1 + nota2) / 2.0;
+                if (media < notaMinima) {
+                    alunosReprovados++;
+                    System.out.println(nomeAluno + " está reprovado com média " + media);
+                }
+            }
+            System.out.println("Média da turma: " + mediaTurma);
+            System.out.println("Número de alunos reprovados: " + alunosReprovados);
+
+            stream.close();
+            inputStream.close();
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            System.out.println("Erro ao adicionar notas" + e.getMessage());
         }
+
+
+        System.out.println("-------------Leitura do Arquivo----------------");
         File file = new File("c:\\temp\\DadosAlunos.txt");
         Scanner scanner1 = new Scanner(file);
 
         while (scanner1.hasNextLine()) {
             System.out.println(scanner1.nextLine());
-
-            int media = 0;
-            int reprovados = 0;
-
-            if (nota_1)
         }
     }
 }
-
