1d983467035dfe706f10de4b3e9fb0b7cc6c7bed
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-08 09:57:20 -0300

    Dois objetos criados a partir da classe pessoa depois realuzar calcula de idades

--- src/main/java/Exercicios/Exercicios_Logica_OO/Basico/Pessoa_App.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Basico/Pessoa_App.java
@@ -1,2 +1,18 @@
-package Exercicios.Exercicios_Logica_OO.Basico;public class Pessoa_App {
+package Exercicios.Exercicios_Logica_OO.Basico;
+
+public class Pessoa_App {
+    public static void main(String[] args) {
+        Pessoa albert = new Pessoa(0, 14,3,1879);
+        Pessoa isaac = new Pessoa(0, 4,1,1643);
+
+         int diaAtual = 8;
+         int mesAtual = 5;
+         int anoAtual = 2024;
+
+         albert.calculaIdade(diaAtual,mesAtual,anoAtual);
+         isaac.calculaIdade(diaAtual, mesAtual, anoAtual);
+
+        System.out.println("Idade de Albert Einsten " + albert.informaIdade());
+        System.out.println("Idade de Isaac Newton " + isaac.informaIdade());
+    }
 }
