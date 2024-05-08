e3780a6bc5be8cb6f42ce10fe22053f713057ee4
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-08 12:05:40 -0300

    Criada class universidade com getters e setters e dois construtores, sendo um deles vazio

--- src/main/java/Exercicios/Exercicios_Logica_OO/Basico/Universidade.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Basico/Universidade.java
@@ -1,2 +1,21 @@
-package Exercicios.Exercicios_Logica_OO.Basico;public class Universidade {
+package Exercicios.Exercicios_Logica_OO.Basico;
+
+public class Universidade {
+    String nome;
+
+    public Universidade(String nome) {
+        this.nome = nome;
+    }
+
+    public Universidade() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
 }
