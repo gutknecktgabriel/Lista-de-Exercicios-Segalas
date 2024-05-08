d912b285c3ea16cd961080620b381990db2cbdbd
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-08 13:59:31 -0300

    Universidade extende da classe departamento

--- src/main/java/Exercicios/Exercicios_Logica_OO/Basico/Universidade.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Basico/Universidade.java
@@ -1,12 +1,23 @@
 package Exercicios.Exercicios_Logica_OO.Basico;
 
-public class Universidade {
+import java.util.ArrayList;
+
+public class Universidade extends ArrayList<Departamento> {
     String nome;
-    
+    Departamento departamento;
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
 
     public Universidade(String nome) {
         this.nome = nome;
     }
+
     public Universidade() {
     }
 
