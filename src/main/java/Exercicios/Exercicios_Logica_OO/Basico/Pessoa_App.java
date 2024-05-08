0cee85e5a666c9eb3b00d994813d47681efe4c21
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-08 12:07:06 -0300

    Criado objetos universidades e apresentado para cada objeto pessoa e apresentado suas informações

--- src/main/java/Exercicios/Exercicios_Logica_OO/Basico/Pessoa_App.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Basico/Pessoa_App.java
@@ -2,8 +2,10 @@ package Exercicios.Exercicios_Logica_OO.Basico;
 
 public class Pessoa_App {
     public static void main(String[] args) {
-        Pessoa albert = new Pessoa(0, 14,3,1879);
-        Pessoa isaac = new Pessoa(0, 4,1,1643);
+        Universidade universidade = new Universidade("Princeton");
+        Pessoa albert = new Pessoa(0, 14,3,1879,"Albert",universidade);
+        Universidade universidade2 = new Universidade("Cambridge");
+        Pessoa isaac = new Pessoa(0, 4,1,1643, " ", universidade2);
 
          int diaAtual = 8;
          int mesAtual = 5;
@@ -14,5 +16,8 @@ public class Pessoa_App {
 
         System.out.println("Idade de Albert Einsten " + albert.informaIdade());
         System.out.println("Idade de Isaac Newton " + isaac.informaIdade());
+        System.out.println("Albert Einstein professor em " + universidade.getNome());
+        System.out.println("Isaac Newton professor em " + universidade2.getNome());
+        System.out.println(albert.retornaUniversadeEPessoa());
     }
 }
