3eb446b685b16ad75898845556ec8806d369d4f6
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-09 12:08:05 -0300

    Problema relacionado ao nome de Issac Newton nao estar sendo visualizado resolvido

--- src/main/java/Exercicios/Exercicios_Logica_OO/Basico/Pessoa_App.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Basico/Pessoa_App.java
@@ -1,20 +1,28 @@
 package Exercicios.Exercicios_Logica_OO.Basico;
 
+import java.util.Scanner;
+
 public class Pessoa_App {
     public static void main(String[] args) {
-        Universidade universidade = new Universidade("Princeton");
-        Departamento departamento = new Departamento("Seguraça");
-        Pessoa albert = new Pessoa(0, 14,3,1879,"Albert",universidade, departamento);
-        Universidade universidade2 = new Universidade("Cambridge");
+        Scanner scanner = new Scanner(System.in);
+        Departamento departamento1 = new Departamento("Direção");
         Departamento departamento2 = new Departamento("Contabilidade");
-        Pessoa isaac = new Pessoa(0, 4,1,1643, " ", universidade2,departamento2);
 
-         int diaAtual = 8;
-         int mesAtual = 5;
-         int anoAtual = 2024;
+        Universidade universidade = new Universidade("Princeton");
+        Pessoa albert = new Pessoa(0, 14, 3, 1879, "Albert Einstein", universidade, departamento1);
 
-         albert.calculaIdade(diaAtual,mesAtual,anoAtual);
-         isaac.calculaIdade(diaAtual, mesAtual, anoAtual);
+        Universidade universidade2 = new Universidade("Cambridge");
+        Pessoa isaac = new Pessoa(0, 4, 1, 1643, "Isaac Newton", universidade2, departamento2);
+
+
+
+        int diaAtual = 8;
+        int mesAtual = 5;
+        int anoAtual = 2024;
+
+        albert.calculaIdade(diaAtual, mesAtual, anoAtual);
+        isaac.calculaIdade(diaAtual, mesAtual, anoAtual);
+        universidade.setDepartamento(departamento1);
 
         System.out.println("Idade de Albert Einsten " + albert.informaIdade());
         System.out.println("Idade de Isaac Newton " + isaac.informaIdade());
@@ -22,8 +30,11 @@ public class Pessoa_App {
         System.out.println("Isaac Newton professor em " + universidade2.getNome());
         System.out.println(albert.retornaUniversadeEPessoa());
         System.out.println(isaac.retornaUniversadeEPessoa());
-        System.out.println(albert.retornaDepartamentoEPessoa());
-        System.out.println(isaac.retornaDepartamentoEPessoa());
+
+
+        //Talvez será usado com o scanner para o usuário escolher seu setor da Universidade
+//        System.out.println(albert.retornaDepartamentoEPessoa());
+//        System.out.println(isaac.retornaDepartamentoEPessoa());
 
     }
 }
