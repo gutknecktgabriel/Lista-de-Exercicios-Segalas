647615a15db3705c9d32f7f60080009ef56f2f0d
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-09 11:33:04 -0300

    Programa principal, realizar criação dos objetos e apresenta

--- src/main/java/Estrutura_Dados_Java/Contato/Teste.java
+++ src/main/java/Estrutura_Dados_Java/Contato/Teste.java
@@ -3,13 +3,33 @@ package Estrutura_Dados_Java.Contato;
 public class Teste {
     public static void main(String[] args) {
 
-
         Contato contato = new Contato();
-        contato.setNome("Gabriel");
-        contato.setEndereco("Joao Rausch");
-        contato.setTelefone("4002-8922");
+        contato.setNome("Gabriel Gutkneckt");
+
+        Endereco end = new Endereco();
+        end.setNomeRua("Joao Rausch");
+        end.setNumeroCasa("514");
+        end.setBairro("Passo Manso");
+        end.setCidade("Blumenau");
+        end.setEstado("Santa Catarina");
+        end.setCep("89032-570");
+
+        Telefone telefone = new Telefone();
+        telefone.setNumero("4002-8922");
+        telefone.setTipo("Fixo");
+        telefone.setDdd("11");
+
+        contato.setEndereco(end);
+        contato.setTelefone(telefone);
         System.out.println(contato.getNome());
-        System.out.println(contato.getEndereco());
-        System.out.println(contato.getTelefone());
+
+        if (contato != null && contato.getEndereco()!= null){
+            System.out.println(contato.getEndereco().getCidade());
+            System.out.println(contato.getEndereco().getEstado());
+            System.out.println("CEP: "+ contato.getEndereco().getCep());
+        }
+        if (contato != null && contato.getTelefone()!= null){
+            System.out.println(contato.getTelefone().getDdd() + " " + contato.getTelefone().getNumero());
+        }
     }
 }
