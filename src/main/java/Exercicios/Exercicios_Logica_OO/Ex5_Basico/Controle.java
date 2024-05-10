813afde67abaf5889ec7dd55ea5e3581a1696a18
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-10 10:33:43 -0300

    Utilizando array list para guadar as referencias

--- src/main/java/Exercicios/Exercicios_Logica_OO/Ex5_Basico/Controle.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Ex5_Basico/Controle.java
@@ -1,2 +1,23 @@
-package Exercicios.Exercicios_Logica_OO.Ex5_Basico;public class Controle {
+package Exercicios.Exercicios_Logica_OO.Ex5_Basico;
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Controle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Aluno> alunos = new ArrayList<>();
+
+        while (true) {
+            System.out.println("Digite o nome do aluno ou 'fim' para sair do programa");
+            String nome = scanner.nextLine();
+
+            if (nome.equals("fim")) {
+                break;
+            }
+        }
+    }
 }
