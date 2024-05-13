05f4183c725985f305978a61fe4d5020c619c0ae
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-13 08:18:39 -0300

    Processo de representar alunos aprovados e reprovados esta certo, processo de calcular media da turma esta errado

--- src/main/java/Exercicios/Exercicios_Logica_OO/Ex5_Basico/Controle.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Ex5_Basico/Controle.java
@@ -1,23 +1,54 @@
 package Exercicios.Exercicios_Logica_OO.Ex5_Basico;
 
-
 import java.util.ArrayList;
 import java.util.Scanner;
-
 
 public class Controle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         ArrayList<Aluno> alunos = new ArrayList<>();
 
-        while (true) {
-            System.out.println("Digite o nome do aluno ou 'fim' para sair do programa");
-            String nome = scanner.nextLine();
+        int somaNotaAlunos;
+        int alunosReprovados = 0;
+        int divisaoParaMedia = 0;
+        int alunosAprovados = 0;
+        int qntNotas = 0;
 
-            if (nome.equals("fim")) {
+
+        Aluno aluno = new Aluno();
+
+        while (true) {
+            alunos.add(aluno);
+
+            System.out.println("Informe o nome do aluno ou digite 'fim' para encerrar o programa.");
+            aluno.setNomeAluno(scanner.next());
+
+            if (aluno.getNomeAluno().equalsIgnoreCase("Fim")) {
                 break;
             }
+
+            System.out.println("Digite a primeira nota do aluno: ");
+            aluno.setPrimeiraNotaParcial(scanner.nextInt());
+            qntNotas++;
+
+            System.out.println("Digite a segunda nota do aluno: ");
+            aluno.setSegundaNotaParcial(scanner.nextInt());
+            qntNotas++;
+
+
+            somaNotaAlunos = aluno.getPrimeiraNotaParcial() + aluno.getSegundaNotaParcial();
+            divisaoParaMedia = somaNotaAlunos / qntNotas;
+
+            if (divisaoParaMedia < 60){
+                alunosReprovados++;
+            }
+            else {
+                alunosAprovados++;
+            }
+
         }
+        System.out.println("Media da turma: " + divisaoParaMedia);
+        System.out.println("Alunos reprovados: " + alunosReprovados);
+        System.out.println("Alunos aprovados: " + alunosAprovados);
     }
 }
