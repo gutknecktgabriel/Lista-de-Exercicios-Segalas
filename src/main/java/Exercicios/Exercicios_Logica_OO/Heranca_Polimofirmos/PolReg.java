26ceab8809ad76f04b0f1ff4390d0338746cc409
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-14 13:51:41 -0300

    Atividade Referente a poligonos

---
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Heranca_Polimofirmos/PolReg.java
@@ -0,0 +1,12 @@
+package Exercicios.Exercicios_Logica_OO.Heranca_Polimofirmos;
+
+public class PolReg {
+    private int numeroLados;
+    private int tamanhoDoLado;
+
+    public PolReg(int numeroLados, int tamanhoDoLado) {
+        this.numeroLados = numeroLados;
+        this.tamanhoDoLado = tamanhoDoLado;
+    }
+
+}
