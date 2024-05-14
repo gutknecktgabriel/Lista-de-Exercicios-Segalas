5e61145842303fa450a691f007581dc8ee27c407
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-14 08:21:45 -0300

    Interface POO

--- src/main/java/Exercicios/Exercicios_Logica_OO/Calculadora/CalcInterface.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Calculadora/CalcInterface.java
@@ -1,2 +1,13 @@
-package Exercicios.Exercicios_Logica_OO.Calculadora;public class CalcInterface {
+package Exercicios.Exercicios_Logica_OO.Calculadora;
+
+public class CalcInterface {
+    public double recebeOperando(int i){
+        return i;
+    }
+    public char recebeOperacao(char operador) {
+        return operador;
+    }
+    public void visualizarResultado(double resultado) {
+        System.out.println(resultado);
+    }
 }
