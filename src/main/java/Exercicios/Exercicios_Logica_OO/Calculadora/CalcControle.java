9be18a2bb14d5b33cb7120fd9b49a997b14d54d8
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-14 12:23:41 -0300

    Class Control

--- src/main/java/Exercicios/Exercicios_Logica_OO/Calculadora/CalcControle.java
+++ src/main/java/Exercicios/Exercicios_Logica_OO/Calculadora/CalcControle.java
@@ -1,5 +1,57 @@
 package Exercicios.Exercicios_Logica_OO.Calculadora;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class CalcControle {
 
+    CalcDados calcDados = new CalcDados();
+    Scanner scanner = new Scanner(System.in);
+
+    public void executar() {
+        try {
+            System.out.println("===============CALCULADORA===============");
+
+            System.out.println("Digite o primeiro valor: ");
+            calcDados.setOperando1(scanner.nextDouble());
+
+
+            System.out.println("Qual a operação que você deseja executar?");
+
+            System.out.println("Opções de operações: \n'+' para somar \n'-' para subtrair \n'*' para multiplicação \n'/' para divisão ");
+            calcDados.setOperacao(scanner.next().charAt(0));
+
+            while (calcDados.getOperacao() != 's') {
+                System.out.println("Digite o segundo valor: ");
+                calcDados.setOperando2(scanner.nextDouble());
+                double resultado = opera(calcDados.getOperando1(), calcDados.getOperando2(), calcDados.getOperacao());
+                calcDados.setOperando1(resultado);
+                System.out.println("Resultado: " + calcDados.getOperando1());
+                break;
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Informe apenas números!");
+        }
+    }
+
+    public double opera(double operador1, double operador2, char op) {
+        double resultado = 0;
+        switch (op) {
+            case '+':
+                resultado += (operador1 + operador2);
+                break;
+            case '-':
+                resultado += (operador1 - operador2);
+                break;
+            case '*':
+                resultado += (operador1 * operador2);
+                break;
+            case '/':
+                resultado += (operador1 / operador2);
+                break;
+            case 's':
+                System.exit(0);
+        }
+        return resultado;
+    }
 }
