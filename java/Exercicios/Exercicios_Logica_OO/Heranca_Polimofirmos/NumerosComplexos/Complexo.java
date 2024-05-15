7d8fd0ee70b4bc2d7fa679f6a399be7f61783214
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-15 12:21:02 -0300

    Complexo

--- java/Exercicios/Exercicios_Logica_OO/Heranca_Polimofirmos/NumerosComplexos/Complexo.java
+++ java/Exercicios/Exercicios_Logica_OO/Heranca_Polimofirmos/NumerosComplexos/Complexo.java
@@ -2,5 +2,19 @@ package Exercicios.Exercicios_Logica_OO.Heranca_Polimofirmos.NumerosComplexos;
 
 public class Complexo {
     private float real;
-    private float imag;
+    private float imagiaria;
+
+    public Complexo(float real, float imaginaria) {
+        this.real = real;
+        this.imagiaria = imaginaria;
+    }
+    public int modulo(int numComplexo){
+        return numComplexo;
+    }
+    public int angulo(){
+        int numComplexo = 0;
+        double angulo_radianos = Math.atan(numComplexo);
+        double angulo_graus = Math.toDegrees(angulo_radianos);
+        return (int) angulo_graus;
+    }
 }
