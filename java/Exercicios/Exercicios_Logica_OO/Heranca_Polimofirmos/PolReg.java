e36ac1a33d607be77f07d3db0aec6b86d15f0ca3
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-15 11:38:49 -0300

    Poligono reg

--- java/Exercicios/Exercicios_Logica_OO/Heranca_Polimofirmos/PolReg.java
+++ java/Exercicios/Exercicios_Logica_OO/Heranca_Polimofirmos/PolReg.java
@@ -2,6 +2,23 @@ package Exercicios.Exercicios_Logica_OO.Heranca_Polimofirmos;
 
 public class PolReg {
     private int numeroLados;
+
+    public int getNumeroLados() {
+        return numeroLados;
+    }
+
+    public void setNumeroLados(int numeroLados) {
+        this.numeroLados = numeroLados;
+    }
+
+    public int getTamanhoDoLado() {
+        return tamanhoDoLado;
+    }
+
+    public void setTamanhoDoLado(int tamanhoDoLado) {
+        this.tamanhoDoLado = tamanhoDoLado;
+    }
+
     private int tamanhoDoLado;
 
     public PolReg(int numeroLados, int tamanhoDoLado) {
@@ -24,6 +41,10 @@ public class PolReg {
     }
 
     public int calculoArea(){
+        int base = 0;
+        int altura = 0;
+        int area; 
+        area = base * altura / 2;
         return 0;
     }
 }
