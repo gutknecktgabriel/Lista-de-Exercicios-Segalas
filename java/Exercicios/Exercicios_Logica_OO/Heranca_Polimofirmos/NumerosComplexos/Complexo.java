f0703ffd8dfc028a9caca8c11261025a90f68fb6
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-16 08:21:17 -0300

    Class Complexo

--- java/Exercicios/Exercicios_Logica_OO/Heranca_Polimofirmos/NumerosComplexos/Complexo.java
+++ java/Exercicios/Exercicios_Logica_OO/Heranca_Polimofirmos/NumerosComplexos/Complexo.java
@@ -1,25 +1,25 @@
 package Exercicios.Exercicios_Logica_OO.Heranca_Polimofirmos.NumerosComplexos;
 
 public class Complexo {
-    private float real;
+    private float numReal;
     private float numeroImaginario;
 
-    public Complexo(float real, float numeroImaginario) {
-        this.real = real;
+    public Complexo(float numReal, float numeroImaginario) {
+        this.numReal = numReal;
         this.numeroImaginario = numeroImaginario;
     }
 
-    public float getReal() {
-        return real;
+    public float getNumReal() {
+        return numReal;
     }
 
     public float getImagiaria() {
         return numeroImaginario;
     }
     public double modulo(){
-        return Math.sqrt(real * real + numeroImaginario * numeroImaginario);
+        return Math.sqrt(numReal * numReal + numeroImaginario * numeroImaginario);
     }
     public double angulo(){
-        return Math.toDegrees(Math.atan2(numeroImaginario, real));
+        return Math.toDegrees(Math.atan2(numeroImaginario,numReal));
     }
 }
--- java/Exercicios/Exercicios_Logica_OO/Heranca_Polimofirmos/NumerosComplexos/Real.java
+++
@@ -1,8 +0,0 @@
-package Exercicios.Exercicios_Logica_OO.Heranca_Polimofirmos.NumerosComplexos;
-
-public class Real extends Complexo {
-
-    public Real(float real, float imaginaria) {
-        super(real, imaginaria);
-    }
-}
