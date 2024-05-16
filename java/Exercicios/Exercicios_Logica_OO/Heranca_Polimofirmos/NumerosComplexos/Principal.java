8a5767a1d2f07dc6b3b55685381e9a5b07aa1a3f
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-16 13:50:54 -0300

    Principal

--- java/Exercicios/Exercicios_Logica_OO/Heranca_Polimofirmos/NumerosComplexos/Principal.java
+++ java/Exercicios/Exercicios_Logica_OO/Heranca_Polimofirmos/NumerosComplexos/Principal.java
@@ -2,6 +2,14 @@ package Exercicios.Exercicios_Logica_OO.Heranca_Polimofirmos.NumerosComplexos;
 
 public class Principal {
     public static void main(String[] args) {
+        Complexo numeroComplexo = new Complexo(3, 1);
+        System.out.println("Módulo do número complexo: " + numeroComplexo.modulo());
+        System.out.println("Ângulo do número complexo: " + numeroComplexo.angulo() + " graus");
 
+        Real numeroRealPositivo = new Real(5);
+        Real numeroRealNegativo = new Real(-2);
+
+        System.out.println("Número real positivo: " + numeroRealPositivo.sinal());
+        System.out.println("Número real negativo: " + numeroRealNegativo.sinal());
     }
 }
