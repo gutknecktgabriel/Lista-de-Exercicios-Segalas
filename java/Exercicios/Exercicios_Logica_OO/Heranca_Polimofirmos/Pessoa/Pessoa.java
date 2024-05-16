314d15fe59d4bbb2119e6c8cd701c42884e628fb
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-16 14:13:45 -0300

    Pessoa

--- java/Exercicios/Exercicios_Logica_OO/Heranca_Polimofirmos/Pessoa/Pessoa.java
+++ java/Exercicios/Exercicios_Logica_OO/Heranca_Polimofirmos/Pessoa/Pessoa.java
@@ -1,2 +1,15 @@
-package Exercicios.Exercicios_Logica_OO.Heranca_Polimofirmos;public class Pessoa {
+package Exercicios.Exercicios_Logica_OO.Heranca_Polimofirmos.Pessoa;
+
+public class Pessoa {
+    String nome;
+    int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    public void mostrarDados(){
+        System.out.println("Nome da pessoa: " + nome);
+        System.out.println("Idade da pessaoa: " + idade);
+    }
 }
