f2b8473dfd99fc0379eace66f192dbacc11ff90e
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-16 10:18:44 -0300

    Refazendo classe

--- java/Exercicios/Exercicios_Logica_OO/Heranca_Polimofirmos/NumerosComplexos/Principal.java
+++ java/Exercicios/Exercicios_Logica_OO/Heranca_Polimofirmos/NumerosComplexos/Principal.java
@@ -1,2 +1,7 @@
-package Exercicios.Exercicios_Logica_OO.Heranca_Polimofirmos.NumerosComplexos;public class Principal {
+package Exercicios.Exercicios_Logica_OO.Heranca_Polimofirmos.NumerosComplexos;
+
+public class Principal {
+    public static void main(String[] args) {
+
+    }
 }
