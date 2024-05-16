d1f3a18882ce1ab33a15bdfb24972f3ad6dd85b4
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-16 08:36:36 -0300

    CLass Real

---
+++ java/Exercicios/Exercicios_Logica_OO/Heranca_Polimofirmos/NumerosComplexos/Real.java
@@ -0,0 +1,7 @@
+package Exercicios.Exercicios_Logica_OO.Heranca_Polimofirmos.NumerosComplexos;
+
+public class Real extends Complexo {
+    public Real(float numReal, float numeroImaginario) {
+        super(numReal, numeroImaginario);
+    }
+}
