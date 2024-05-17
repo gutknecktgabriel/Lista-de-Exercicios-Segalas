9545a0d28a9853325ccf14e7037caa5838b67645
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-17 09:25:18 -0300

    Principal

--- java/Exercicios/Exercicios_Logica_OO/Estrutura_Dados/Principal.java
+++ java/Exercicios/Exercicios_Logica_OO/Estrutura_Dados/Principal.java
@@ -1,2 +1,12 @@
-package Exercicios.Exercicios_Logica_OO.Estrutura_Dados;public class Principal {
+package Exercicios.Exercicios_Logica_OO.Estrutura_Dados;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
 }
