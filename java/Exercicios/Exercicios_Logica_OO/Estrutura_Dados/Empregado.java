7e93b618a476912f04005ec5e837b6eac374c5ab
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-17 09:27:28 -0300

    Empregado

--- java/Exercicios/Exercicios_Logica_OO/Estrutura_Dados/Empregado.java
+++ java/Exercicios/Exercicios_Logica_OO/Estrutura_Dados/Empregado.java
@@ -5,5 +5,42 @@ public class Empregado {
     private String cpf;
     private double salario;
 
+    public Empregado(String nome, String cpf, double salario) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salario = salario;
+    }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return "Empregado{" +
+                "nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
 }
