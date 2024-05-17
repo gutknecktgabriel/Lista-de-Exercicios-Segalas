3653c95d8ce56fba344db9f73b398544642662b2
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-17 09:24:46 -0300

    Empregado

---
+++ java/Exercicios/Exercicios_Logica_OO/Estrutura_Dados/Empregado.java
@@ -0,0 +1,9 @@
+package Exercicios.Exercicios_Logica_OO.Estrutura_Dados;
+
+public class Empregado {
+    private String nome;
+    private String cpf;
+    private double salario;
+
+
+}
---
+++ java/Exercicios/Exercicios_Logica_OO/Estrutura_Dados/Principal.java
@@ -0,0 +1,2 @@
+package Exercicios.Exercicios_Logica_OO.Estrutura_Dados;public class Principal {
+}
