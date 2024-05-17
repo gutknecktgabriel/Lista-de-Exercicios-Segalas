ac6075281fa16d2a1a1b1ba0dbd04fc05d31d985
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-17 11:33:12 -0300

    Ordenando empregados do menor salario ao maior

--- java/Exercicios/Exercicios_Logica_OO/Estrutura_Dados/Principal.java
+++ java/Exercicios/Exercicios_Logica_OO/Estrutura_Dados/Principal.java
@@ -5,8 +5,29 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Empregado[] empregados = new Empregado[5];
 
+        for (int i = 0; i < empregados.length; i++) {
+            Empregado empregado = new Empregado("", "", 0);
 
+            System.out.println("Informe o nome do empregado " + (i + 1) + " :");
+            empregado.setNome(scanner.nextLine());
 
+            System.out.println("Informe o CPF do empregado " + (i + 1) + " :");
+            empregado.setCpf(scanner.next());
+
+            System.out.println("Informe o salario do empregado " + (i + 1) + " :");
+            empregado.setSalario(scanner.nextDouble());
+            scanner.nextLine();
+            empregados[i] = new Empregado(empregado.getNome(), empregado.getCpf(), empregado.getSalario());
+        }
+
+            bubbleSort(empregados);
+        System.out.println("Empregados ordenados pelo salário:");
+        for (Empregado empregado : empregados) {
+            System.out.println(empregado);
+        }
+    }
+    private static void bubbleSort(Empregado[] empregados) {
     }
 }
