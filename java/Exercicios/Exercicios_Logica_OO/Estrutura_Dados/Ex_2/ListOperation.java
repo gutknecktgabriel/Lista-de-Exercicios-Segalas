1ab3b11dcefd561edf421b775747e2b54c9f595c
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-20 14:10:11 -0300

    ListOperationm ira realizar os processos de adicionar e remover objetos da lista encadeada

--- java/Exercicios/Exercicios_Logica_OO/Estrutura_Dados/Ex_2/ListOperation.java
+++ java/Exercicios/Exercicios_Logica_OO/Estrutura_Dados/Ex_2/ListOperation.java
@@ -1,2 +1,10 @@
-package Exercicios.Exercicios_Logica_OO.Estrutura_Dados.Ex_2;public class ListOperation {
+package Exercicios.Exercicios_Logica_OO.Estrutura_Dados.Ex_2;
+
+public class ListOperation<T> {
+    ListNode n;
+    ListNode anterior;
+
+    public void adicionar(T n, T anterior){
+
+    }
 }
