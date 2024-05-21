200abd26361649407451bd3cd1a0776633439c1f
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-21 10:27:12 -0300

    Metodo add implementado

--- java/Exercicios/Exercicios_Logica_OO/Estrutura_Dados/Ex_2/ListOperation.java
+++ java/Exercicios/Exercicios_Logica_OO/Estrutura_Dados/Ex_2/ListOperation.java
@@ -4,8 +4,16 @@ public class ListOperation<T> {
     ListNode<T> primeiro;
     ListNode<T> ultimo;
 
-    public void add(ListNode n, ListNode ant){
-
+    public void add(ListNode n, ListNode ant) {
+        if (ant == null) {
+            n.proximo = null;
+        } else {
+            n.proximo = ant.proximo;
+            ant.proximo = n;
+        }
+    }
+    public void remove(ListNode n){
 
     }
 }
+
