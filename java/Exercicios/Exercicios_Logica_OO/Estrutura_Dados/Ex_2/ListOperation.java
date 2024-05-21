6c1fd9ca08a0b3d2d1e6126177b2e21aae73dd98
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-21 08:18:13 -0300

    List Operation recebe valores e metodo

--- java/Exercicios/Exercicios_Logica_OO/Estrutura_Dados/Ex_2/ListOperation.java
+++ java/Exercicios/Exercicios_Logica_OO/Estrutura_Dados/Ex_2/ListOperation.java
@@ -1,5 +1,11 @@
 package Exercicios.Exercicios_Logica_OO.Estrutura_Dados.Ex_2;
 
 public class ListOperation<T> {
+    ListNode<T> primeiro;
+    ListNode<T> ultimo;
 
+    public void add(ListNode n, ListNode ant){
+
+
+    }
 }
