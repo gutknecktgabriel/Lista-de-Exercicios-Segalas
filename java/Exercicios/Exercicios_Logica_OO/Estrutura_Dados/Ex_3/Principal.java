fe489f1fa7822c64dab1e2d1ce7755e99c07589c
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-22 10:24:14 -0300

    Program main

--- java/Exercicios/Exercicios_Logica_OO/Estrutura_Dados/Ex_3/Principal.java
+++ java/Exercicios/Exercicios_Logica_OO/Estrutura_Dados/Ex_3/Principal.java
@@ -1,2 +1,15 @@
-package Exercicios.Exercicios_Logica_OO.Estrutura_Dados.Ex_3;public class Principal {
+package Exercicios.Exercicios_Logica_OO.Estrutura_Dados.Ex_3;
+
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char[] simbolosMat = {'*', '/', '%', '+', '-'};
+
+        Infixa_Para_Posfixa posfixa = new Infixa_Para_Posfixa<>(100);
+
+
+    }
 }
