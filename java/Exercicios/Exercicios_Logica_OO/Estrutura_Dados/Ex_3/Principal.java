598c52abc54f16dfc4032546efd757ba3119fdb8
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-22 14:54:48 -0300

    Principal ainda nao finalizado

--- java/Exercicios/Exercicios_Logica_OO/Estrutura_Dados/Ex_3/Principal.java
+++ java/Exercicios/Exercicios_Logica_OO/Estrutura_Dados/Ex_3/Principal.java
@@ -1,15 +1,31 @@
 package Exercicios.Exercicios_Logica_OO.Estrutura_Dados.Ex_3;
 
-
-import java.util.Scanner;
-
-public class Principal {
+public class Principal<S> {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        char[] simbolosMat = {'*', '/', '%', '+', '-'};
+        String contaMatematica = "( 96 + 3 ) * 89 / 5";
+        System.out.println(contaMatematica);
+    }
 
-        Infixa_Para_Posfixa posfixa = new Infixa_Para_Posfixa<>(100);
+    public static Infixa_Para_Posfixa<String> converter(String infixa) {
+        String vetorCharInfixa[] = infixa.split(" ");
+        Principal<String> pilha = new Principal<>();
+        Principal<String> posFixo = new Principal<>();
 
+        System.out.println("Tamanho da conta: " + infixa.length());
+        int contagem = 0;
+        int topoDaPilha = 0;
+        String opcoesDeOperacoes = null;
 
+        while (contagem < vetorCharInfixa.length) {
+            switch (opcoesDeOperacoes) {
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+            }
+
+        }
+        return null;
     }
 }
+
