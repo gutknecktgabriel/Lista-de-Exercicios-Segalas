be4d3f59f1ff300cee234142910e679445d7f798
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-27 11:02:37 -0300

    Verificado erro onde na classe de busca por elemento estava comparando o no atual com ele mesmo ao invés de estar comparando com o elementi

--- java/POO_Praticas/Lista/ListaEncadeada.java
+++ java/POO_Praticas/Lista/ListaEncadeada.java
@@ -5,13 +5,13 @@ public class ListaEncadeada<T> {
     private No<T> ultimo;
     private int tamanho;
     private String str;
+    private final int NAO_ENCONTRADO = -1;
 
-    public void adiciona(T elemento){
+    public void adiciona(T elemento) {
         No<T> celula = new No<T>(elemento);
-        if (this.tamanho == 0){
+        if (this.tamanho == 0) {
             this.inicio = celula;
-        }
-        else {
+        } else {
             this.ultimo.setProximo(celula);
         }
         this.ultimo = celula;
@@ -27,23 +27,56 @@ public class ListaEncadeada<T> {
         this.tamanho = tamanho;
     }
 
-    public void limpa(){
+    public void limpa() {
 
-        for (No<T> atual = this.inicio; atual != null;) {
+        for (No<T> atual = this.inicio; atual != null; ) {
             No<T> proximo = atual.getProximo();
             atual.setElemento(null);
             atual.setProximo(null);
             atual = proximo;
         }
-        
+
         this.inicio = null;
         this.ultimo = null;
         this.tamanho = 0;
     }
 
+    private No<T> buscaNo(int posicao) {
+        if (!(posicao > 0 && posicao <= this.tamanho)) {
+            throw new IllegalArgumentException("Posição não existe.");
+        }
+
+        No<T> noAtual = this.inicio;
+        for (int i = 0; i < posicao; i++) {
+            noAtual = noAtual.getProximo();
+
+        }
+
+        return noAtual;
+    }
+
+
+    public T buscaPorPosicao(int posicao) {
+        return this.buscaNo(posicao).getElemento();
+    }
+
+    public int buscaPorElemento(T elemento) {
+        No<T> noAtual = this.inicio;
+        int pos = 0;
+        while (noAtual != null) {
+            if (noAtual.getElemento().equals(elemento)) {
+                return pos;
+            }
+            pos++;
+            noAtual = noAtual.getProximo();
+        }
+
+        return NAO_ENCONTRADO;
+    }
+
     @Override
     public String toString() {
-        if (this.tamanho == 0){
+        if (this.tamanho == 0) {
             return "[]";
         }
 
