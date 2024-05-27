77dd1c9f1a2b773f592a990a14e32720a2844df0
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-27 12:17:40 -0300

    Melhorada visibilidadade da classe main, implementado metodo de adicionar valores na posicao desejada

--- java/POO_Praticas/Lista/Teste/Principal.java
+++ java/POO_Praticas/Lista/Teste/Principal.java
@@ -4,6 +4,38 @@ import POO_Praticas.Lista.ListaEncadeada;
 
 public class Principal {
     public static void main(String[] args) {
+//        testesIniciais();
+//        adicionaInicio();
+        adicionaNaPosicao();
+    }
+
+    public static void adicionaNaPosicao(){
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+        lista.adiciona(1);
+        lista.adiciona(2);
+        lista.adiciona(4);
+
+        lista.adicionarNaPosicao(0,0);
+        lista.adicionarNaPosicao(4,5);
+        lista.adicionarNaPosicao(2,3);
+
+        System.out.println(lista);
+
+    }
+
+
+    public static void adicionaInicio() {
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+        lista.adicionaInicio(3);
+        lista.adicionaInicio(2);
+        lista.adicionaInicio(1);
+
+        System.out.println(lista);
+    }
+
+
+    public static void testesIniciais() {
+
         ListaEncadeada<Integer> lista = new ListaEncadeada();
         lista.adiciona(1);
         System.out.println(lista);
@@ -35,5 +67,8 @@ public class Principal {
         System.out.println(lista.buscaPorPosicao(1));
         System.out.println(lista.buscaPorPosicao(2));
         System.out.println(lista.buscaPorPosicao(3));
+
     }
+
 }
+
