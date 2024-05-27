c1ec0f9cab93d349fc304304d3ee6dbcae47a4c3
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-27 08:50:07 -0300

    Classe Principal, puxa metodo 'adiciona' e adiciona elementos na lista

--- java/POO_Praticas/Lista/Teste/Principal.java
+++ java/POO_Praticas/Lista/Teste/Principal.java
@@ -2,12 +2,25 @@ package POO_Praticas.Lista.Teste;
 
 import POO_Praticas.Lista.ListaEncadeada;
 
-public class ListaEncadeadaTeste {
+public class Principal {
     public static void main(String[] args) {
         ListaEncadeada<Integer> lista = new ListaEncadeada();
         lista.adiciona(1);
-        lista.adiciona(2);
+        System.out.println(lista);
 
+        lista.adiciona(2);
+        System.out.println(lista);
+
+        lista.adiciona(3);
+        System.out.println(lista);
+
+        lista.adiciona(4);
+        System.out.println(lista);
+
+        lista.adiciona(5);
+        System.out.println(lista);
+
+        lista.adiciona(6);
         System.out.println(lista);
 
     }
