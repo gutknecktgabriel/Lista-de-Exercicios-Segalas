35101950ae71263df96c336d2e30f6cacf9c1d11
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-27 12:15:35 -0300

    Ajuste de codigom, ajustado posicao e deixando o codigo mais limpo

--- java/POO_Praticas/Lista/ListaEncadeada.java
+++ java/POO_Praticas/Lista/ListaEncadeada.java
@@ -7,6 +7,14 @@ public class ListaEncadeada<T> {
     private String str;
     private final int NAO_ENCONTRADO = -1;
 
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(int tamanho) {
+        this.tamanho = tamanho;
+    }
+
     public void adiciona(T elemento) {
         No<T> celula = new No<T>(elemento);
         if (this.tamanho == 0) {
@@ -16,15 +24,6 @@ public class ListaEncadeada<T> {
         }
         this.ultimo = celula;
         this.tamanho++;
-    }
-
-
-    public int getTamanho() {
-        return tamanho;
-    }
-
-    public void setTamanho(int tamanho) {
-        this.tamanho = tamanho;
     }
 
     public void limpa() {
@@ -51,10 +50,8 @@ public class ListaEncadeada<T> {
             noAtual = noAtual.getProximo();
 
         }
-
         return noAtual;
     }
-
 
     public T buscaPorPosicao(int posicao) {
         return this.buscaNo(posicao).getElemento();
@@ -72,6 +69,35 @@ public class ListaEncadeada<T> {
         }
 
         return NAO_ENCONTRADO;
+    }
+
+    public void adicionaInicio(T elemento) {
+        if (this.tamanho == 0) {
+            No<T> novoNo = new No<>(elemento);
+            this.inicio = novoNo;
+            this.ultimo = novoNo;
+        } else {
+            No<T> novoNo = new No<>(elemento, this.inicio);
+            this.inicio = novoNo;
+        }
+        this.tamanho++;
+    }
+
+    public void adicionarNaPosicao(int posicao, T elemento) {
+        if (posicao < 0 || posicao > this.tamanho) {
+            throw new IllegalArgumentException("Impossível adicionar, verifique a posição mencionada ou o tamanho da lista");
+        }
+        if (posicao == 0) { //lista vazia
+            this.adicionaInicio(elemento);
+        } else if (posicao == this.tamanho) { // adiciona
+            this.adiciona(elemento);
+        } else { // adicionar no meio da lista
+            No<T> noAnterior = this.buscaNo(posicao);
+            No<T> proximoNo = noAnterior.getProximo();
+            No<T> novoNo = new No<>(elemento, proximoNo);
+            noAnterior.setProximo(novoNo);
+            this.tamanho++;
+        }
     }
 
     @Override
