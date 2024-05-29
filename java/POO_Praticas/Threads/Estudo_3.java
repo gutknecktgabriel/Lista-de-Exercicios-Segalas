b3b2a2b8cda7b4225165d465ce2c362e5fc748ce
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-29 10:29:31 -0300

    Aplicando metodos na classe

--- java/POO_Praticas/Threads/Estudo_3.java
+++ java/POO_Praticas/Threads/Estudo_3.java
@@ -3,18 +3,27 @@ package POO_Praticas.Threads;
 public class Estudo_3 extends Thread {
     public static int cont = 0;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Estudo_3 thread = new Estudo_3();
         thread.start();
 
         while (thread.isAlive()) {
             System.out.println("Aguarde...");
+            thread.sleep(1000);
             System.out.println("Contador desatualizado: " + cont);
 
             cont++;
             System.out.println("Contador atualizado: " + cont);  //atualiza o valor e imprime
+
+            System.out.println("ID thread: " + thread.getId());
+            System.out.println("Interrupted thread: " + thread.isInterrupted());
+            thread.setName("Gabss");
+            System.out.println("Name thread modified to: " + thread.getName());
+
+
         }
     }
+
     public void run() {
         cont++;
     }
