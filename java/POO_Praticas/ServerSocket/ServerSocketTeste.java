5e2a172a0edc2f0cc059191eabe8d006300620f5
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-29 14:53:37 -0300

    Servidor Local

--- java/POO_Praticas/ServerSocket/ServerSocketTeste.java
+++ java/POO_Praticas/ServerSocket/ServerSocketTeste.java
@@ -1,18 +1,31 @@
 package POO_Praticas.ServerSocket;
 
 import java.io.IOException;
-import java.net.InetAddress;
-
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ServerSocketTeste {
     public static void main(String[] args) throws IOException {
-        java.net.ServerSocket server = new java.net.ServerSocket(3322);
-        System.out.println("Servidor iniciado na porta 3322");
-        InetAddress inet = server.getInetAddress();
-        System.out.println("HostAddres="+inet.getHostAddress());
-        System.out.println("HostName="+inet.getHostName());
-        System.out.println("Porta = "+server.getLocalPort());
-        byte[] b = InetAddress.getByName("localhost").getAddress();
-        System.out.println(b[0] + "." + b[1] + "." + b[2] + "." + b[3]);
+        try {
+            ServerSocket server = new ServerSocket(3322);
+            System.out.println("Servidor iniciado na porta 3322");
+
+            Socket cliente = server.accept();
+            System.out.println("Cliente conectado do IP " + cliente.getInetAddress().getHostAddress());
+
+            Scanner scanner = new Scanner(cliente.getInputStream());
+
+            while (scanner.hasNextLine()){
+                System.out.println(scanner.hasNextLine());
+            }
+            scanner.close();
+            server.close();
+
+        } catch (IOException e) {
+            Logger.getLogger(ServerSocket.class.getName()).log(Level.SEVERE, null, e);
+        }
     }
 }
