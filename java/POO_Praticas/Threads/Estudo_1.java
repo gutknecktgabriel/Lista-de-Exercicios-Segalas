07ab1e9ef6c9e8c86b728faeac1a93b28b94487c
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-29 09:21:40 -0300

    Pequeno contador utilizando thread e o metodo sleep para apresentar mensagens

--- java/POO_Praticas/Threads/Estudo_1.java
+++ java/POO_Praticas/Threads/Estudo_1.java
@@ -1,2 +1,24 @@
-package POO_Praticas.Threads;public class Estudo_1 {
+package POO_Praticas.Threads;
+
+public class Estudo_1 extends Thread {
+    public static void main(String[] args) throws InterruptedException {
+        Estudo_1 thread = new Estudo_1();
+        thread.start();
+        thread.sleep(3000);
+
+        int cont = 0;
+
+        System.out.println("Esse codigo ta rodando fora da thread.");
+        for (int i = 0; i <10 ; i++) {
+            thread.sleep(300);
+            cont++;
+            System.out.println("Quantas vezes a contagem foi realizada: " + cont);
+        }
+
+    }
+    public void run(){
+        //rodando o codigo aqui primeiro
+        System.out.println("Esse codigo está rodando dentro de uma thread");
+
+    }
 }
