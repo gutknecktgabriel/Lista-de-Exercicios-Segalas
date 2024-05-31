d0fa2c25bf64b6e0510525fa3fcbea28b7889a2e
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-31 13:49:09 -0300

    Thread Cliente inicio

--- java/POO_Praticas/ServerCommunication/ThreadCliente.java
+++ java/POO_Praticas/ServerCommunication/ThreadCliente.java
@@ -1,2 +1,8 @@
-package POO_Praticas.ServerCommunication;public class ThreadCliente {
+package POO_Praticas.ServerCommunication;
+
+import java.net.Socket;
+
+public class ThreadCliente extends Thread{
+    private Socket cliente;
+
 }
