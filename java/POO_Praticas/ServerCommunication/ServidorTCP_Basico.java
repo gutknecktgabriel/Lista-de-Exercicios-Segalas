cbd3251e98f93163db41a5b08f44c905dc4ce7f7
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-31 11:34:26 -0300

    Server TCP básico

---
+++ java/POO_Praticas/ServerCommunication/ServidorTCP_Basico.java
@@ -0,0 +1,32 @@
+package POO_Praticas.ServerCommunication;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Date;
+
+public class ServidorTCP_Basico {
+    public static void main(String[] args) throws UnknownHostException {
+        try {
+            ServerSocket server = new ServerSocket(4578);
+            System.out.println("Server iniciado na porta: 4578");
+            while (true){
+                Socket usuario = server.accept();
+                System.out.println("Usuário conectado: " + usuario.getInetAddress().getHostAddress());
+                ObjectOutputStream saida = new ObjectOutputStream(usuario.getOutputStream());
+                saida.flush();
+                saida.writeObject(new Date());
+                saida.close();
+                usuario.close();
+            }
+        }
+        catch (IOException e){
+            System.out.println("Ocorreu um erro ao tentar iniciar o servidor: " + e.getMessage());
+        }
+        finally {
+
+        }
+    }
+}
