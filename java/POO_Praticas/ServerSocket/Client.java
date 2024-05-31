eac5d34cf587199050421fa7d94382f45f1a2370
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-31 11:05:49 -0300

    Classe cliente, recebe mensagem do usuario, retorna mensagem caso o servidor nao seja inicializado ou conectado corretamente

--- java/POO_Praticas/ServerSocket/Client.java
+++ java/POO_Praticas/ServerSocket/Client.java
@@ -1,31 +1,119 @@
 package POO_Praticas.ServerSocket;
 
-import javax.swing.*;
 import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ConnectException;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Client extends javax.swing.JFrame {
+class FClient extends javax.swing.JFrame {
+
     private Socket cliente;
 
-    /////////////////////////ADICIONAR DEPOIS public FClient() {
-    //        initComponents();
-    //        initCliente();
-    //    }
-    private void initClient(){
-        try {
-            cliente = new Socket("127.0.0.1",3322);
-        }
-        catch (IOException e){
-            Logger.getLogger(Client.class.getName()).log(Level.SEVERE,null, e);
-        }
+    public FClient() {
+        initComponents();
+        initCliente();
     }
-    private void initComponents() {
-        JScrollPane jScrollPane1 = new javax.swing.JScrollPane();
-        JTextArea jTextArea1 = new javax.swing.JTextArea();
-        JButton jButton1 = new javax.swing.JButton();
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+    private void initCliente() {
+        try {
+            cliente = new Socket("192.168.0.59", 3322);
+        } catch (ConnectException ex) {
+            Logger.getLogger(FClient.class.getName()).log(Level.SEVERE, "Não foi possível conectar ao servidor: " + ex.getMessage(), ex);
+        } catch (IOException ex) {
+            Logger.getLogger(FClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+
+    @SuppressWarnings("unchecked")
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jButton1 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        jButton1.setText("Enviar Mensagem");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 376, Short.MAX_VALUE)
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addComponent(jButton1)
+                                                .addGap(0, 0, Short.MAX_VALUE)))
+                                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 228, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jButton1)
+                                .addContainerGap(25, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
+        if (cliente == null) {
+            Logger.getLogger(FClient.class.getName()).log(Level.SEVERE, "Cliente não inicializado.");
+            return;
+        }
+
+        try {
+            PrintStream saida = new PrintStream(cliente.getOutputStream());
+            saida.println(jTextArea1.getText());
+            jTextArea1.setText("");
+        } catch (IOException ex) {
+            Logger.getLogger(FClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void main(String args[]) {
+
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(FClient.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(FClient.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(FClient.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(FClient.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FClient().setVisible(true);
+            }
+        });
+    }
+
+    private javax.swing.JButton jButton1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
 }
