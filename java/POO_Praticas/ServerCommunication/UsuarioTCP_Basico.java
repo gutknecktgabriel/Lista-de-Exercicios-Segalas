a1977872c1b83c242fad9ae0854597d4f525a800
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-05-31 11:40:49 -0300

    Usuario TCP

---
+++ java/POO_Praticas/ServerCommunication/UsuarioTCP_Basico.java
@@ -0,0 +1,22 @@
+package POO_Praticas.ServerCommunication;
+
+import javax.swing.*;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.util.Date;
+
+public class UsuarioTCP_Basico {
+    public static void main(String[] args) {
+        try {
+            Socket usuario = new Socket("Gabs",4578);
+            ObjectInputStream entrada = new ObjectInputStream(usuario.getInputStream());
+            Date data_atual = (Date) entrada.readObject();
+            JOptionPane.showMessageDialog(null,"Data recevida do servidor: " + data_atual.toString());
+            entrada.close();
+            System.out.println("Conexão encerrada");
+        }
+        catch (Exception e){
+            System.out.println("Não possível iniciar a conexão com o usuário: " + e.getMessage());
+        }
+    }
+}
