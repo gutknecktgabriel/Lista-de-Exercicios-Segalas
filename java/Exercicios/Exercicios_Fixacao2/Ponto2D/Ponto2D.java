53aaa618e09a71cfce9ea082dfb007115767175b
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-06-03 11:11:11 -0300

    Removido parâmetros getters e setters

--- java/Exercicios/Exercicios_Fixacao2/Ponto2D/Ponto2D.java
+++ java/Exercicios/Exercicios_Fixacao2/Ponto2D/Ponto2D.java
@@ -18,19 +18,4 @@ public class Ponto2D {
         this.y = ponto.y;
     }
 
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
 }
