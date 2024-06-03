c309d4361c8864ae56a93038c6707b16165d6508
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-06-03 09:48:36 -0300

    Classe Principal contador

--- java/Exercicios/Exercicios_Fixacao2/Contador/Principal.java
+++ java/Exercicios/Exercicios_Fixacao2/Contador/Principal.java
@@ -1,2 +1,17 @@
-package Exercicios.Exercicios_Fixacao2.Contador;public class Principal {
+package Exercicios.Exercicios_Fixacao2.Contador;
+
+public class Principal {
+    public static void main(String[] args) {
+        Contador contador = new Contador();
+
+        System.out.println("Valor do contador: " + contador.getValor());
+
+        contador.incrementar();
+        System.out.println("Contador com valor incrementado: " + contador.getValor());
+
+        contador.zerar();
+        System.out.println("Contador zerado: " + contador.getValor());
+
+        System.out.println("Retornando valor do contador: " + contador.retornarValor());
+    }
 }
