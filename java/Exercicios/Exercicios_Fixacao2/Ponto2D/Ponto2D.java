2b85856da8ea4aeb8e9dab06f544bfab6068134a
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-06-03 11:43:38 -0300

    Metodos refeitos

--- java/Exercicios/Exercicios_Fixacao2/Ponto2D/Ponto2D.java
+++ java/Exercicios/Exercicios_Fixacao2/Ponto2D/Ponto2D.java
@@ -4,22 +4,49 @@ public class Ponto2D {
     private double x;
     private double y;
 
-    public Ponto2D(){
-        this.x = 0;
-        this.y = 0;
+    public Ponto2D() {
+        this.x = 0.0;
+        this.y = 0.0;
     }
 
     public Ponto2D(double x, double y) {
         this.x = x;
         this.y = y;
     }
-    public Ponto2D(Ponto2D ponto){
+
+    public Ponto2D(Ponto2D ponto) {
         this.x = ponto.x;
         this.y = ponto.y;
     }
-    public Ponto2D(double movimentacaoPontos){
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void moverPontos(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
+    public void moverPontos(Ponto2D ponto) {
+        this.x = ponto.x;
+        this.y = ponto.y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
 }
