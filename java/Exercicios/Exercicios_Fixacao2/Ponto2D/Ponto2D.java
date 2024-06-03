cbcc1a8c19266fa3bdd1c49e0380f086ebf1aa59
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-06-03 10:21:58 -0300

    Variaveis criadas e inicializadas

--- java/Exercicios/Exercicios_Fixacao2/Ponto2D/Ponto2D.java
+++ java/Exercicios/Exercicios_Fixacao2/Ponto2D/Ponto2D.java
@@ -1,5 +1,28 @@
 package Exercicios.Exercicios_Fixacao2.Ponto2D;
 
 public class Ponto2D {
+    private double pontoHorizontal;
+    private double pontoVertical;
+
+    public Ponto2D(double pontoHorizontal, double pontoVertical) {
+        this.pontoHorizontal = pontoHorizontal;
+        this.pontoVertical = pontoVertical;
+    }
+
+    public double getPontoHorizontal() {
+        return pontoHorizontal;
+    }
+
+    public void setPontoHorizontal(double pontoHorizontal) {
+        this.pontoHorizontal = pontoHorizontal;
+    }
+
+    public double getPontoVertical() {
+        return pontoVertical;
+    }
+
+    public void setPontoVertical(double pontoVertical) {
+        this.pontoVertical = pontoVertical;
+    }
 
 }
