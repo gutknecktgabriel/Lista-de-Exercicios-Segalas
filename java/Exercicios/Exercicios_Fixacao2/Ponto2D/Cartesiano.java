a33d673982b14a413df9efd96a45be36f5232518
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-06-03 14:32:09 -0300

    Recebendo valores do usuário

--- java/Exercicios/Exercicios_Fixacao2/Ponto2D/Cartesiano.java
+++ java/Exercicios/Exercicios_Fixacao2/Ponto2D/Cartesiano.java
@@ -1,2 +1,18 @@
-package Exercicios.Exercicios_Fixacao2.Ponto2D;public class Cartesiano {
+package Exercicios.Exercicios_Fixacao2.Ponto2D;
+
+import java.util.Scanner;
+
+public class Cartesiano {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Ponto2D ponto = new Ponto2D();
+
+        System.out.print("Informe o valor que deseja aplicar ao X no plano cartesiano: ");
+        ponto.setX(scanner.nextDouble());
+
+        System.out.print("Informe o valor que deseja aplicar ao Y no plano cartesiano: ");
+        ponto.setY(scanner.nextDouble());
+
+
+    }
 }
