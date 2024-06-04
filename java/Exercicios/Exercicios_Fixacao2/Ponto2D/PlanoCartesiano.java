4a06f0fd81b2fa8052954dcea4d4b9ea421214f2
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-06-04 08:21:32 -0300

    Plano cartesiano

--- java/Exercicios/Exercicios_Fixacao2/Ponto2D/PlanoCartesiano.java
+++ java/Exercicios/Exercicios_Fixacao2/Ponto2D/PlanoCartesiano.java
@@ -4,21 +4,19 @@ public class PlanoCartesiano {
     public static void main(String[] args) {
         Ponto2D p1 = new Ponto2D(2,6);
         Ponto2D p2 = new Ponto2D(3,4);
-        Ponto2D p3 = new Ponto2D(1,5);
 
         System.out.println("Ponto P1: " + p1);
         System.out.println("Ponto P2: " + p2);
-        System.out.println("Ponto P3: " + p3);
 
-        System.out.println("Distância entre os pontos P2 e P3: " + p2.calcularDistancia(p3));
+        System.out.println("Distância entre os pontos P1 e P2: " + p1.calcularDistancia(p2));
 
         p1.moverPontos(5, 6);
-        System.out.println("Ponto 1 movido: " + p1);
+        System.out.println("Ponto 1 movido! localização atual: " + p1);
 
-        Ponto2D p4 = p1.clone();
-        System.out.println("Clone de Ponto 1: " + p4);
+        Ponto2D p3 = p1.clone();
+        System.out.println("Clone do Ponto 1: " + p3);
 
-        System.out.println("Ponto 1 é igual ao clone? " + p1.equals(p4));
+        System.out.println("O clone é igual ao Ponto 1? " + p1.equals(p3));
     }
 }
 
