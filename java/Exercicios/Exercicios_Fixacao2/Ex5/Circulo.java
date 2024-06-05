c7dc27b29ffe2ec7f830301dc212a5d55253f64a
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-06-05 11:58:40 -0300

    Circulo

--- java/Exercicios/Exercicios_Fixacao2/Ex5/Circulo.java
+++ java/Exercicios/Exercicios_Fixacao2/Ex5/Circulo.java
@@ -9,4 +9,26 @@ public class Circulo {
         this.ponto = ponto;
     }
 
+    public Circulo(double raio) {
+        this.raio = ponto;
+    }
+
+    public double getRaio() {
+        return raio;
+    }
+
+    public void inflar() {
+        raio = +(32);
+        ponto = +(88);
+    }
+
+    public void inflarUmaUnidade() {
+        raio++;
+        ponto++;
+    }
+
+    public void desinflarUmaUnidade() {
+        raio--;
+        ponto--;
+    }
 }
