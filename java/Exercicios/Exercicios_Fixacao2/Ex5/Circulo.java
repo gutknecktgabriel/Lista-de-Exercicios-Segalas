68611ecba148019b58a813ef94e24f12bb2b0918
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-06-05 14:17:50 -0300

    Circulo

--- java/Exercicios/Exercicios_Fixacao2/Ex5/Circulo.java
+++ java/Exercicios/Exercicios_Fixacao2/Ex5/Circulo.java
@@ -1,34 +1,66 @@
 package Exercicios.Exercicios_Fixacao2.Ex5;
 
 public class Circulo {
-    private double raio;
-    private double ponto;
 
-    public Circulo(double raio, double ponto) {
+    private double raio;
+    private double x;
+    private double y;
+
+    public Circulo(double raio, double x, double y) {
         this.raio = raio;
-        this.ponto = ponto;
+        this.x = x;
+        this.y = y;
     }
 
     public Circulo(double raio) {
-        this.raio = ponto;
+        this(raio, 0, 0);
     }
 
     public double getRaio() {
         return raio;
     }
 
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
+
+    public void inflar(double valor) {
+        this.raio += valor;
+    }
+
+    public void desinflar(double valor) {
+        this.raio -= valor;
+        if (this.raio < 0) {
+            this.raio = 0;
+        }
+    }
+
     public void inflar() {
-        raio = +(32);
-        ponto = +(88);
+        inflar(1);
     }
 
-    public void inflarUmaUnidade() {
-        raio++;
-        ponto++;
+    public void desinflar() {
+        desinflar(1);
     }
 
-    public void desinflarUmaUnidade() {
-        raio--;
-        ponto--;
+    public void mover() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public void mover(double novoX, double novoY) {
+        this.x = novoX;
+        this.y = novoY;
+    }
+
+    public void mover(Circulo outro) {
+        this.x = outro.x;
+        this.y = outro.y;
+    }
+
+    @Override
+    public String toString() {
+        return "Circulo [raio=" + raio + ", centro=(" + x + ", " + y + ")]";
     }
 }
+
---
+++ java/Exercicios/Exercicios_Fixacao2/Ex5/Main.java
@@ -0,0 +1,2 @@
+package Exercicios.Exercicios_Fixacao2.Ex5;public class Main {
+}
