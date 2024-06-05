586aae9d52c52da36ed7d7a498b488975717099b
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-06-05 11:37:09 -0300

    Circulo

---
+++ java/Exercicios/Exercicios_Fixacao2/Ex5/Circulo.java
@@ -0,0 +1,12 @@
+package Exercicios.Exercicios_Fixacao2.Ex5;
+
+public class Circulo {
+    private double raio;
+    private double ponto;
+
+    public Circulo(double raio, double ponto) {
+        this.raio = raio;
+        this.ponto = ponto;
+    }
+
+}
