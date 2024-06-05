2518f08a76fe6fa9292713a681e3333231b27c46
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-06-05 11:33:01 -0300

    Main

--- java/Exercicios/Exercicios_Fixacao2/Ex4/Main.java
+++ java/Exercicios/Exercicios_Fixacao2/Ex4/Main.java
@@ -1,2 +1,22 @@
-package Exercicios.Exercicios_Fixacao2.Ex4;public class Main {
+package Exercicios.Exercicios_Fixacao2.Ex4;
+
+public class Main {
+    public static void main(String[] args) {
+        Reta reta1 = new Reta(2, 3);
+        Reta reta2 = new Reta(1, 5, 3, 9);
+
+        System.out.println("Reta 1: " + reta1);
+        System.out.println("Reta 2: " + reta2);
+
+        Reta interseccaoReta = new Reta(1, 2);
+        Reta outraReta = new Reta(3, -4);
+
+        Reta.Ponto pontoInterseccao = interseccaoReta.interseccao(outraReta);
+        if (pontoInterseccao != null) {
+            System.out.println("Ponto de interseção: " + pontoInterseccao);
+        } else {
+            System.out.println("As retas são paralelas, portanto, não existe ponto de interseção.");
+        }
+    }
 }
+
