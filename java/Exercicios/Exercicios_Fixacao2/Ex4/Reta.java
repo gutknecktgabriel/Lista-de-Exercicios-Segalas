8c8a64d84c06e95273c4fbc76cbcb7e804bd0990
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-06-05 10:27:44 -0300

    Reta]

---
+++ java/Exercicios/Exercicios_Fixacao2/Ex4/Reta.java
@@ -0,0 +1,41 @@
+package Exercicios.Exercicios_Fixacao2.Ex4;
+
+public class Reta {
+    private double coeAngular;
+    private double coeLinear;
+    private int ponto1;
+    private int ponto2;
+
+    public double getCoeAngular() {
+        return coeAngular;
+    }
+
+    public void setCoeAngular(double coeAngular) {
+        this.coeAngular = coeAngular;
+    }
+
+    public double getCoeLinear() {
+        return coeLinear;
+    }
+
+    public void setCoeLinear(double coeLinear) {
+        this.coeLinear = coeLinear;
+    }
+
+    public int getPonto1() {
+        return ponto1;
+    }
+
+    public void setPonto1(int ponto1) {
+        this.ponto1 = ponto1;
+    }
+
+    public int getPonto2() {
+        return ponto2;
+    }
+
+    public void setPonto2(int ponto2) {
+        this.ponto2 = ponto2;
+    }
+
+}
