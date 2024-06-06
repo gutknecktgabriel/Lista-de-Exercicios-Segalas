25912ced70619d7f5d94367e8fa56a2da9a551d4
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-06-06 14:14:19 -0300

    Main

---
+++ java/Exercicios/Exercicios_Fixacao2/Ex7/Main.java
@@ -0,0 +1,8 @@
+package Exercicios.Exercicios_Fixacao2.Ex7;
+
+public class Main {
+    public static void main(String[] args) {
+        Continente continente = new Continente("Europa");
+
+    }
+}
