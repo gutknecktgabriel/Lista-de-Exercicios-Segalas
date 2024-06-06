c125a9cc7c1824818101d0c245da68d5da38936a
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-06-06 11:59:22 -0300

    Classe Auxiliar País

--- java/Exercicios/Exercicios_Fixacao2/Ex7/Pais.java
+++ java/Exercicios/Exercicios_Fixacao2/Ex7/Pais.java
@@ -1,5 +1,25 @@
 package Exercicios.Exercicios_Fixacao2.Ex7;
 
 public class Pais {
+    private String nomePais;
+    private int populacao;
+    private double dimensao;
 
+    public Pais(String nomePais, int populacao, double dimensao) {
+        this.nomePais = nomePais;
+        this.populacao = populacao;
+        this.dimensao = dimensao;
+    }
+
+    public String getNomePais() {
+        return nomePais;
+    }
+
+    public int getPopulacao() {
+        return populacao;
+    }
+
+    public double getDimensao() {
+        return dimensao;
+    }
 }
