91cc33c4bc93c86ee01adf9cf595f4dfd7800629
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-06-06 10:32:53 -0300

    Ex6

--- java/Exercicios/Exercicios_Fixacao2/Ex6/Main.java
+++ java/Exercicios/Exercicios_Fixacao2/Ex6/Main.java
@@ -2,15 +2,17 @@ package Exercicios.Exercicios_Fixacao2.Ex6;
 
 public class Main {
     public static void main(String[] args) {
-        Pais pais = new Pais("66498-1","Qatar", 214341131,8483481);
-        Pais pais2 = new Pais("66498-1","Brasil", 51351152,565216513);
-        Pais pais3 = new Pais("66498-1","Mexico", 2141131,74755656);
+        Pais pais = new Pais("66498-1","Qatar", 211131,82112181);
+        Pais pais2 = new Pais("66498-1","Brasil", 521131152,565216513);
+
 
         System.out.println("País: " + pais.getNome());
         System.out.println("Código ISO:" + pais.getCodIso());
         System.out.println("Dimensão: " + pais.getDimensao());
         System.out.println("População: " + pais.getPopulacao());
-
+        System.out.println("Densidade: " + pais.densidade());
+        System.out.println("Verificar igualdade: " + pais.verificarIgualdade(pais2));
+        System.out.println("O " + pais.getNome() + " é limítrofe do " + pais2.getNome() + " ? " + pais.limitrofe(pais2));
 
 
     }
