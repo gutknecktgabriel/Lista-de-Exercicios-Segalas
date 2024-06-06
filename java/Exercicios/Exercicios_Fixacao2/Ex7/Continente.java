13fba00d18c317f10c21a85f32cf660d42b5e6b7
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-06-06 11:12:35 -0300

    Continente

--- java/Exercicios/Exercicios_Fixacao2/Ex7/Continente.java
+++ java/Exercicios/Exercicios_Fixacao2/Ex7/Continente.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 public class Continente {
     private String nomeContinente;
     private ArrayList<String> paises;
+    private double dimensaoContinente;
+    private int populacaoContinente;
 
     public Continente(String nomeContinente) {
         this.nomeContinente = nomeContinente;
@@ -20,8 +22,35 @@ public class Continente {
         this.nomeContinente = nomeContinente;
     }
 
+    public double getDimensaoContinente() {
+        return dimensaoContinente;
+    }
+
+    public void setDimensaoContinente(double dimensaoContinente) {
+        this.dimensaoContinente = dimensaoContinente;
+    }
+
+    public int getPopulacaoContinente() {
+        return populacaoContinente;
+    }
+
+    public void setPopulacaoContinente(int populacaoContinente) {
+        this.populacaoContinente = populacaoContinente;
+    }
+
     public void adicionarPaises(Pais pais) {
         pais.add(pais);
     }
 
+    public double dimensaoContinente() {
+        return dimensaoContinente;
+    }
+
+    public int populacaoContinente() {
+        return populacaoContinente;
+    }
+
+    public double densidadePoulacional() {
+        return this.populacaoContinente / this.densidadePoulacional();
+    }
 }
