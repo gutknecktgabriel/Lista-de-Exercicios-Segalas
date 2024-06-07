28213b58878d1e6d5d272933ae026d33bcbb53a1
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-06-07 13:38:40 -0300

    Main

--- java/Exercicios/Exercicios_Fixacao2/Ex8/Main.java
+++ java/Exercicios/Exercicios_Fixacao2/Ex8/Main.java
@@ -1,2 +1,21 @@
-package Exercicios.Exercicios_Fixacao2.Ex8;public class Main {
+package Exercicios.Exercicios_Fixacao2.Ex8;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Pessoa pessoa = new Pessoa("Maria");
+        Pessoa pessoa2 = new Pessoa("Maria");
+
+        System.out.print("Informe o nome da sua mãe: ");
+        pessoa.setMae(scanner.next());
+
+        System.out.print("Informe o nome da sua mãe: ");
+        pessoa2.setMae(scanner.next());
+
+        System.out.println("É a mesma pessoa ? " + pessoa.getMae().equals(pessoa2.getMae()));
+
+    }
 }
