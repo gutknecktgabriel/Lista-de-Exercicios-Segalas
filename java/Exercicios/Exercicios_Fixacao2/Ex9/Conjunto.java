d61945abefe8a9ec7d46d37f341f9f92a35d58aa
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-06-10 11:43:45 -0300

    Conjunto

--- java/Exercicios/Exercicios_Fixacao2/Ex9/Conjunto.java
+++ java/Exercicios/Exercicios_Fixacao2/Ex9/Conjunto.java
@@ -1,4 +1,42 @@
 package Exercicios.Exercicios_Fixacao2.Ex9;
 
+import java.util.List;
+
 public class Conjunto {
+    private List<String> conjunto;
+    private int adicionarQtd;
+
+    public Conjunto(List<String> elemento, int adicionarQtd) {
+        this.conjunto = elemento;
+        this.adicionarQtd = adicionarQtd;
+    }
+
+    public List<String> getConjunto() {
+        return conjunto;
+    }
+
+    public void setConjunto(List<String> conjunto) {
+        this.conjunto = conjunto;
+    }
+
+    public int getAdicionarQtd() {
+        return adicionarQtd;
+    }
+
+    public void setAdicionarQtd(int adicionarQtd) {
+        this.adicionarQtd = adicionarQtd;
+    }
+
+    public void adicionar() {
+        if (conjunto.contains(conjunto)){
+            System.out.println("O elemento já existe na lista");
+        }
+        else {
+            conjunto.add(String.valueOf(adicionarQtd));
+        }
+    }
+
+    public void verificarExistencia(){
+
+    }
 }
