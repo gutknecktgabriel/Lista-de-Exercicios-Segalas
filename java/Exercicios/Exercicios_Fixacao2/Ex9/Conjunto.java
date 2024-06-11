b9caf0ccf653eee272e66b00d13085c8d00b66f8
Gabriel Gutkneckt <gabriel.gutkneckt@segalas.com.br>
2024-06-11 08:09:46 -0300

    Conjunto

--- java/Exercicios/Exercicios_Fixacao2/Ex9/Conjunto.java
+++ java/Exercicios/Exercicios_Fixacao2/Ex9/Conjunto.java
@@ -1,101 +1,59 @@
+
 package Exercicios.Exercicios_Fixacao2.Ex9;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Conjunto {
-    private int adicionaQtd;
-    private int removerQtd;
     private List<String> conjunto;
-    private String elemento;
-    private String[] elementos;
-
 
     public Conjunto() {
-    }
-
-    public String getElemento() {
-        return elemento;
-    }
-
-    public void setElemento(String elemento) {
-        this.elemento = elemento;
-    }
-
-    public int getAdicionaQtd() {
-        return adicionaQtd;
-    }
-
-    public void setAdicionaQtd(int adicionaQtd) {
-        this.adicionaQtd = adicionaQtd;
-    }
-
-    public String[] getElementos() {
-        return elementos;
-    }
-
-    public void setElementos(String[] elementos) {
-        this.elementos = elementos;
-    }
-
-    public int getRemoverQtd() {
-        return removerQtd;
-    }
-
-    public void setRemoverQtd(int removerQtd) {
-        this.removerQtd = removerQtd;
+        this.conjunto = new ArrayList<>();
     }
 
     public void adicionar(String elemento) {
-        if (conjunto.contains(elemento)) {
-            System.out.println("O elemento já existe na lista");
+        if (!conjunto.contains(elemento)) {
+            conjunto.add(elemento);
         } else {
-            conjunto.add((elemento));
-        }
-        adicionaQtd++;
-    }
-
-    public void remover(String elemento) {
-        if (conjunto.contains(elemento)) {
-            System.out.println("Error");
-        } else {
-            conjunto.remove(String.valueOf(elemento));
-            removerQtd--;
+            System.out.println("O elemento já existe no conjunto");
         }
     }
 
     public boolean verificarExistencia(String elemento) {
-        return elemento.contains(elemento);
+        return conjunto.contains(elemento);
     }
 
     public Conjunto uniao(Conjunto outro) {
         Conjunto resultado = new Conjunto();
-        for (String elemento : this.elementos) {
+        for (String elemento : this.conjunto) {
             resultado.adicionar(elemento);
         }
-        for (String elemento : outro.elementos) {
+        for (String elemento : outro.conjunto) {
             resultado.adicionar(elemento);
         }
         return resultado;
     }
 
     public Conjunto inter(Conjunto outro) {
-        Conjunto resultadoInter = new Conjunto();
-        for (String elemento : this.elementos) {
-            resultadoInter.verificarExistencia(elemento);
+        Conjunto resultado = new Conjunto();
+        for (String elemento : this.conjunto) {
+            if (outro.verificarExistencia(elemento)) {
+                resultado.adicionar(elemento);
+            }
         }
-        for (String elemento : outro.elementos) {
-            verificarExistencia(elemento);
-        }
-        return resultadoInter;
+        return resultado;
     }
 
-    public void remover() {
-        if (conjunto.contains(elemento)) {
-            System.out.println("O elemento já existe na lista");
-        } else {
-            conjunto.remove(String.valueOf(elemento));
-            adicionaQtd--;
+    public Conjunto menos(Conjunto outro) {
+        Conjunto resultado = new Conjunto();
+        for (String elemento : this.conjunto) {
+            if (!outro.verificarExistencia(elemento)) {
+                resultado.adicionar(elemento);
+            }
         }
+        return resultado;
+    }
+    public void exibir() {
+        System.out.println(conjunto);
     }
 }
-
